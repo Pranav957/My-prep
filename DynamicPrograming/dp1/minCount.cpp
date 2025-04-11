756b6ecf3f10f4389b30567e092bc7469c7e3081
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-11 12:39:41 +0530

    Update minCount.cpp

--- DynamicPrograming/dp1/minCount.cpp
+++ DynamicPrograming/dp1/minCount.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
-int minCount(int n)
+int squareSum(int n)
 {
-	//Write your code here
-	if(n<=3)
-	  return n;
-
-int min=INT_MAX;
-	  for(int i=1;i<=sqrt(n);i++)
-	  {
-		  int v=i*i;
-		   int x=1+minCount(n-v);
-         if(x<min)
-		   min=x;
-	  }
-	  return min;
+    if(n<=1)
+      return n;
+      
+      int min=INT_MAX;
+      for(int i=1;i<sqrt(n);i++)
+      {
+          int x=i*i;
+          int ans=1+squareSum(n-x);
+          if(ans<min)
+            min=ans;
+          
+      }
+      return min;
+    
 }
