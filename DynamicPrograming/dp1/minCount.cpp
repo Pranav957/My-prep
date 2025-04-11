f0b18519be09e00e82c541896554dc544ff1470a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-11 12:48:23 +0530

    Update minCount.cpp

--- DynamicPrograming/dp1/minCount.cpp
+++ DynamicPrograming/dp1/minCount.cpp
@@ -16,3 +16,26 @@ int squareSum(int n)
       return min;
     
 }
+
+int squareSum(int n)
+{
+   
+    
+    int* arr=new int[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        int min=INT_MAX;
+        for(int j=1;j<sqrt(i);j++)
+        {
+            int x=j*j;
+            int ans=1+arr[i-x];
+            if(ans<min)
+              min=ans;
+        }
+        arr[i]=min;
+    }
+    return arr[n];
+    
+}
