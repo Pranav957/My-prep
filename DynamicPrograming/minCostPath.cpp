1fc459de40fd7ac5b9021e034e6c0b4c409ba4ef
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-11 18:34:27 +0530

    Update minCostPath.cpp

--- DynamicPrograming/minCostPath.cpp
+++ DynamicPrograming/minCostPath.cpp
@@ -25,3 +25,73 @@ int minCostPath(int **input, int m, int n)
 
   return helper(input,0,0,m,n,dp);
 }
+
+
+int minCostPath(int arr[][3],int m,int n,int i,int j)
+{
+    // if(i==m-1 && j==n-1)
+    // {
+    //     return arr[i][j];
+    // }
+    
+    // if(i>=m || j>=n)
+    // {
+    //     return INT_MAX;
+    // }
+    
+    // int a=minCostPath(arr,m,n,i+1,j+1);
+    // int b=minCostPath(arr,m,n,i+1,j);
+    // int c=minCostPath(arr,m,n,i,j+1);
+    
+    // return arr[i][j]+min({a,b,c});
+    
+    int** output=new int*[m];
+    for(int i=0;i<m;i++)
+    {
+        output[i]=new int[n];
+    }
+    
+    output[m-1][n-1]=arr[m-1][n-1];
+    
+    for(int i=m-1;i>=0;i--)
+    {
+        int a=INT_MAX,b=INT_MAX,c=INT_MAX;
+        for(int j=n-1;j>=0;j--)
+        {
+            if(i==m-1 && j==n-1)
+              continue;
+              
+              if(i+1<m && j+1<n)
+                a=output[i+1][j+1];
+              if(i+1<m)  
+              b=output[i+1][j];
+              if(j+1<n)
+                c=output[i][j+1];
+                
+            output[i][j]= arr[i][j]+min({a,b,c});        
+        }
+    }
+    return output[0][0];
+   
+    
+}
+
+int main()
+{
+    // int m,n;
+    // cin >> m >> n;
+    int m=3,n=3;
+    
+    int arr[][3]={{1,2,3},{4,5,6},{6,7,8}};
+    // int** arr=new int*[m];
+    // for(int i=0;i<m;i++)
+    // {
+    //     arr[i]=new int[n];
+    //     for(int j=0;j<n;j++)
+    //       cin >> arr[i][j];
+    // }
+    int a= minCostPath(arr,n,m,0,0);
+    cout<<a<<" ";
+
+    return 0;
+}
