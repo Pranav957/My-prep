bea257981424a2342236b6466219a49a4c8aa131
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-11 11:51:39 +0530

    Update stairCase.cpp bottom-up

--- DynamicPrograming/dp1/stairCase.cpp
+++ DynamicPrograming/dp1/stairCase.cpp
@@ -14,6 +14,22 @@ int countMinSteps(int n)
   int c=countMinSteps(n-3);
   return (a+b+c);
 }
+
+int bottom-up(int n){
+int* arr=new int[n+1];
+    arr[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        int a=arr[i-1],b=0,c=0;
+        if(i-2>=0)
+         b=arr[i-2];
+        if(i-3>=0)
+          c=arr[i-3];
+          
+        arr[i]=a+b+c;  
+    }
+    return arr[n];
+}
 int main()
 {
     // take input from the user.
