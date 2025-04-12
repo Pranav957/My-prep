2f6c4c50af9eef1fef9103fa7465385b30a9d777
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-12 15:40:17 +0530

    Update waysTomakeCoinChange.cpp

--- DynamicPrograming/waysTomakeCoinChange.cpp
+++ DynamicPrograming/waysTomakeCoinChange.cpp
@@ -1,47 +1,51 @@
-int countWaysToMakeChange(int denominations[], int numDenominations, int value,int** arr){
+#include<bits/stdc++.h>
+int countWaysToMakeChange(int denominations[], int numDenominations, int value,int dp[][1001]){
 	//Write your code here
-	if(value==0)
-	    return 1;
-    if(value<0)
-	   return 0;
-	   if(arr[value][numDenominations]!=-1)
-	        return arr[value][numDenominations];
-	 if(numDenominations<=0)
-	    return 0;  		 
-	arr[value][numDenominations]= countWaysToMakeChange(denominations,numDenominations-1,value,arr) + countWaysToMakeChange(denominations,numDenominations,value-denominations[numDenominations-1],arr);
-	 return arr[value][numDenominations];
-}
-int countWaysToMakeChangeS(int denominations[],int numDenominations,int value)
-{
-	int ** arr=new int*[value+1];
-	for(int i=0;i<=value;i++)
-	{
-		arr[i]=new int[numDenominations+1];
-		for(int j=0;j<=numDenominations;j++)
-		   arr[i][j]=-1;
-	}
-  return  countWaysToMakeChange( denominations,  numDenominations, value,arr);
-}
-int countWaysToMakeChange(int denominations[], int numDenominations, int value)
-{
-  int **arr = new int *[value + 1];
-  for (int i = 0; i <= value; i++) {
-    arr[i] = new int[numDenominations ];
-    for (int j = 0; j < numDenominations; j++)
-      arr[i][j] = -1;
-  }
-  for(int j=0;j<numDenominations;j++)
-      arr[0][j]=1;
-for(int i=1;i<=value;i++)
-{
-	for(int j=0;j<numDenominations;j++)
-	{
-		int x=(i-denominations[j]>=0)? arr[i-denominations[j]][j]:0;
-		int y=(j>=1)?arr[i][j-1]:0;
-		arr[i][j]=x+y;
-	}
+	 if(value==0)
+		     return 1;
+
+	if(numDenominations==0 )
+	  return 0;
+   
+   if(dp[numDenominations][value]!=-1)
+    return dp[numDenominations][value];
+	  if(denominations[numDenominations-1]>value)
+	     return dp[numDenominations][value]= countWaysToMakeChange(denominations,numDenominations-1,value,dp);
+
+     int a=countWaysToMakeChange(denominations,numDenominations,value-denominations[numDenominations-1],dp);
+	 int b=countWaysToMakeChange(denominations,numDenominations-1,value,dp);
+	 return dp[numDenominations][value]= a+b;
 }
 
-return arr[value][numDenominations-1];
-  
+int countWaysToMakeChangeH(int denominations[], int numDenominations, int value){
+	int ** dp=new int*[numDenominations+1];
+	for(int i=0;i<numDenominations+1;i++)
+	  dp[i]=new int[value+1];
+
+	 for(int i=0;i<numDenominations+1;i++)
+	 {
+		  dp[i][0]=1;
+	 } 
+	 for(int j=0;j<=value;j++)
+	 {
+		 dp[0][j]=0;
+	 }
+	 for(int i=1;i<numDenominations+1;i++)
+	 {
+		 for(int j=1;j<value+1;j++)
+		 {
+			  if(denominations[i-1]>j)
+			    dp[i][j]=dp[i-1][j];
+			  else
+			   dp[i][j]=dp[i][j-denominations[i-1]]+dp[i-1][j];	
+		 }
+	 }
+	 return dp[numDenominations][value];
+}
+
+int countWaysToMakeChange(int denominations[], int numDenominations, int value){
+	//Write your code here
+	int dp[11][1001];
+	memset(dp,-1,sizeof(dp));
+   return  countWaysToMakeChange(denominations,numDenominations,value,dp);
 }
