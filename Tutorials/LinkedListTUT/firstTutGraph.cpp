91920f4f116ad8d7782073749f9e7542cc4a6b83
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-14 07:55:15 +0530

    Update firstTutGraph.cpp

--- Tutorials/LinkedListTUT/firstTutGraph.cpp
+++ Tutorials/LinkedListTUT/firstTutGraph.cpp
@@ -155,3 +155,53 @@ int main() {
       cout<<"true";
       return 0;
 }
+**********************************************************************************************ALL CONNECTED COMPONENTS****************************************************************************
+  #include <bits/stdc++.h>
+#include<vector>
+using namespace std;
+void Traversal_DFS(int **arr, int n, bool *visited, int sv,vector<int>& v) {
+  
+  v.push_back(sv);
+  visited[sv] = true;
+  for (int i = 0; i < n; i++) {
+    if (arr[sv][i] == 1 && visited[i] == false)
+      Traversal_DFS(arr, n, visited, i,v);
+  }
+  
+}
+int main() {
+  // Write your code here
+  int n, e;
+  cin >> n >> e;
+  if(n==0)
+    return 0;
+  int **arr = new int *[n];
+  for (int i = 0; i < n; i++) {
+    arr[i] = new int[n];
+    for (int j = 0; j < n; j++)
+      arr[i][j] = 0;
+  }
+  for (int i = 0; i < e; i++) {
+    int a, b;
+    cin >> a >> b;
+    arr[a][b] = 1;
+    arr[b][a] = 1;
+  }
+  bool *visited = new bool[n];
+  for (int i = 0; i < n; i++)
+    visited[i] = false;
+
+     for (int j = 0; j < n; j++) {
+       if (visited[j] == false)
+        {
+          vector<int> v;
+         Traversal_DFS(arr, n, visited, j, v);
+         sort(v.begin(),v.end());
+         for (int i = 0; i < v.size(); i++)
+           cout << v[i] << " ";
+          //delete v;
+       }
+       cout<<endl;
+   }
+}
+
