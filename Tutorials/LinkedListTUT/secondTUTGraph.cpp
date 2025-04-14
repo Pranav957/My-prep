764a24a7cd45e189e63f31a61f37532311aeae21
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-14 07:38:41 +0530

    Update secondTUTGraph.cpp

--- Tutorials/LinkedListTUT/secondTUTGraph.cpp
+++ Tutorials/LinkedListTUT/secondTUTGraph.cpp
@@ -67,3 +67,78 @@ int main() {
     }
     delete[] edges;
 }
+
+**************************************************************************************************************************************************************************************************************
+                                                        //BY SENDING VECTOR
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> get_path(int** edges, int n,int st,int ed,bool* visited)
+{
+
+    if(st==ed)
+    {
+        vector<int> v;
+        v.push_back(st);
+        return v;
+    }
+
+    visited[st]=true;
+
+    for(int i=0;i<n;i++)
+    {
+        if(!visited[i] && edges[st][i]==1)
+        {
+            vector<int> v=get_path(edges,n,i,ed,visited);
+            if(!v.empty())
+            {
+                v.push_back(st);
+                return v;
+            }
+        }
+    }
+    return vector<int>();
+}
+
+
+int main() {
+    // Write your code here
+    int n,e;
+    cin >> n >> e;
+
+    int** edges=new int*[n];
+    for(int i=0;i<n;i++)
+     {
+          edges[i]=new int[n];
+          for(int j=0;j<n;j++)
+            edges[i][j]=0;
+     }
+
+     for(int i=0;i<e;i++)
+     {
+        int f,s;
+        cin >> f >> s;
+        edges[f][s]=1;
+        edges[s][f]=1;
+     }
+
+      bool* visited=new bool[n];
+     for(int i=0;i<n;i++)
+      visited[i]=false;
+     
+     int v1, v2;
+     cin >> v1 >> v2;
+    vector<int> v=get_path(edges,n,v1,v2,visited);
+    if(v.size()==0)
+     return 0;
+    for(int i=0;i<v.size();i++)
+      cout<<v.at(i)<<" ";
+
+    delete [] visited;
+    for(int i=0;i<n;i++)
+      delete [] edges[i];
+
+      delete [] edges;
+      
+}
+
