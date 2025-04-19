e74b1cb736431a2bfe239a9ca84f6841b59e22bd
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-19 16:31:31 +0530

    Create replacepi.cpp

---
+++ Tutorials/CharArrayTUT/replacepi.cpp
@@ -0,0 +1,26 @@
+int getLength(char input [])
+{
+	if(input[0]=='\0')
+	  return 0;
+
+	  return 1+getLength(input+1);
+}
+void replacePi(char input[]) {
+	// Write your code here
+	if(input[0]=='\0')
+	  return ;
+
+	  replacePi(input+1);
+	  if(input[0]=='p' && input[1]=='i')
+	   {
+		  for(int i=getLength(input);i>=0;i--)
+		  {
+			  input[i+2]=input[i];
+		  }
+		  input[0]='3';
+		  input[1]='.';
+		  input[2]='1';
+		  input[3]='4';
+	   }
+
+}
