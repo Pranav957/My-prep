26147697013a9442efa3e2b1a8caf6b3ff37130c
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-19 17:18:35 +0530

    Update firstTUT.cpp

--- Tutorials/CharArrayTUT/firstTUT.cpp
+++ Tutorials/CharArrayTUT/firstTUT.cpp
@@ -135,5 +135,29 @@ bool checkAB(char input[]) {
     }
 
 }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+int getLength(char input[])
+{
+    if(input[0]=='\0')
+      return 0;
+
+      return 1+getLength(input+1);
+}
+void pairStar(char input[]) {
+    // Write your code here
+    if(input[1]=='\0')
+      return ;
+
+      pairStar(input+1);
+    if(input[0]==input[1])
+     {
+        int length=getLength(input);
+        for(int i=length;i>=1;i--)
+          input[i+1]=input[i];
+           input[1]='*';
+     }
+    return;
+
+}
 
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
