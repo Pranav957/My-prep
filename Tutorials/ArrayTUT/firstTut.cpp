994a9c265bc88e5e964bd8d5a5553dabdf3b4e81
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-20 16:03:18 +0530

    Update firstTut.cpp

--- Tutorials/ArrayTUT/firstTut.cpp
+++ Tutorials/ArrayTUT/firstTut.cpp
@@ -14,3 +14,21 @@
     }
   
 }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<UNIQUE IN ARRAY<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  int findUnique(int *arr, int size)
+{
+    //Write your code here
+     for(int i=0;i<size;i++)
+     {
+         bool a=false;
+         for(int j=0;j<size;j++)
+         {
+             if(i==j)
+                 continue;
+             else if(arr[i]==arr[j])
+                  a=true;
+         } 
+         if(a==false)
+             return arr[i];
+     } 
+} 
