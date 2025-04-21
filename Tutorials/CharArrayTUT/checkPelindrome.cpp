c1e85800dae467550157d13fd8e4e6495190f596
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-21 16:45:20 +0530

    Create checkPelindrome.cpp

---
+++ Tutorials/CharArrayTUT/checkPelindrome.cpp
@@ -0,0 +1,16 @@
+bool checkPalindrome(char str[]) {
+    // Write your code here
+    int i=0,x=0;
+    while(str[x]!='\0')
+        x++;
+    x=x-1;
+
+    while(i<x)
+    {
+        if(str[i]!=str[x])
+           return false;
+        x--;
+        i++;   
+    }    
+    return true;
+}
