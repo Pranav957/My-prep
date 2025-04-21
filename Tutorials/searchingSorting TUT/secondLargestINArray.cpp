5b42155c362dabac07d680910fe847c90095c265
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-21 12:25:18 +0530

    Create secondLargestINArray.cpp

---
+++ Tutorials/searchingSorting TUT/secondLargestINArray.cpp
@@ -0,0 +1,18 @@
+#include<climits>
+int findSecondLargest(int arr[], int n)
+{
+    //Write your code here
+    int largetst=INT_MIN;
+    int secondLarge;
+    for(int i=0;i<n;i++)
+    {
+       if(arr[i]>largetst)
+          {
+              secondLarge=largetst;
+              largetst=arr[i];
+          }
+         else if(arr[i]>secondLarge)
+           secondLarge=arr[i]; 
+    }
+    return secondLarge;
+}
