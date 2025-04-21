342c751cd513d4b9f5c94c12b4ccc13e1d28fecd
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-21 17:19:18 +0530

    Update TutorialOne.cpp

--- Tutorials/CharArrayTUT/TutorialOne.cpp
+++ Tutorials/CharArrayTUT/TutorialOne.cpp
@@ -16,6 +16,20 @@ void reverseString(char input[])
     }
 }
 
+void trimSpaces(char input[]) {
+    // Write your code here
+    int i=0, j=0;
+    for(int i=0; input[i]!='\0';i++)
+    {
+        if(input[i]!=' ')
+        {
+            input[j]=input[i];
+            j++;
+        }
+    }
+    input[j]='\0';
+}
+
 void replaceCharacter(char input[], char c1, char c2) {
     // Write your code here
      int i=0;
