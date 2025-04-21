94e937d856204f889b7af41d5228ee36d7abeb04
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-21 12:43:22 +0530

    Create sort 012.cpp

---
+++ Tutorials/searchingSorting TUT/sort 012.cpp
@@ -0,0 +1,26 @@
+void sort012(int *arr, int n)
+{
+    //Write your code here
+    int nz=0,nt=n-1, i=0;
+    while(i<=nt)
+    {
+      if(arr[i]==0)
+      {
+          int temp=arr[i];
+          arr[i]=arr[nz];
+          arr[nz]=temp;
+          i++;
+          nz++;
+      } 
+      else if(arr[i]==2)
+      {
+          int temp=arr[i];
+          arr[i]=arr[nt];
+          arr[nt]=temp;
+          nt--;
+      }
+        else
+            i++;
+     
+    }
+}
