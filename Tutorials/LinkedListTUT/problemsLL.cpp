052a51068c7712df35a84c778583bdf053308a97
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-22 16:55:14 +0530

    Update problemsLL.cpp

--- Tutorials/LinkedListTUT/problemsLL.cpp
+++ Tutorials/LinkedListTUT/problemsLL.cpp
@@ -27,3 +27,57 @@ int findNode(Node *head, int n){
    }
    return -1;
 }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<EliminateDuplicates<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Node *removeDuplicates(Node *head)
+{
+   if(head==NULL)
+	   return head;
+	Node* temp=head;
+	while(head->next!=NULL)
+	{
+		if(head->data==head->next->data)
+		{
+			Node * deleteNode=head->next;
+			head->next=head->next->next;
+			delete deleteNode;
+		}
+		else{
+			head=head->next;
+		}
+	}
+	return temp;
+}
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<PrintReversedLL<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+void printReverse(Node *head)
+{
+    //Write your code here
+	if(head==NULL)
+	  return;
+	 printReverse(head->next);
+	 cout<<head->data<<" "; 
+}
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<CheckIfPalindrome<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+bool isPalindrome(Node *head,int a[],int i) 
+{
+	static int j=0;
+    if(head==NULL)
+	     return true;
+	a[i]=head->data;
+	bool k=isPalindrome(head->next,a,++i);
+	if(k==false)
+	   return k;
+	else if(head->data==a[j])
+	       {
+          j++;
+          return true; 
+      }
+	else 
+	   return false;		   
+
+
+}
+bool isPalindrome(Node *head) {
+  // Write your code here
+  int *a=new int[100000];
+  return isPalindrome(head,a,0);
+}
