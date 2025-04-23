7795b00b63fe4f6236c686091b5d3d6b8783b87f
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-23 17:48:58 +0530

    Update allUniqueCodesFromString.cpp

--- Tutorials/RecursionTUT/allUniqueCodesFromString.cpp
+++ Tutorials/RecursionTUT/allUniqueCodesFromString.cpp
@@ -43,4 +43,38 @@ int getCodes(string input, string output[10000]) {
     }
     return a+b;
 }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<PRINT ALL CODES<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    #include <string.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+void printAllPossibleCodes(string input,string output) {
+    /*
+    Given the input as a string, print all its possible combinations. You do not need to return anything.
+    */
+    if(input.length()==0)
+    {
+        cout<<output<<endl;
+        return;
+    }
+    int x=input[0]-'0';
+    char ch='a'+x-1;
+    printAllPossibleCodes( input.substr(1),output+ch);
+    if(input[1]!='\0')
+    {
+        int x2=input[1]-'0';
+        x2=10*x+x2;
+        if(x2>=10 && x2<=26)
+        {
+        char ch2='a'+x2-1;
+        printAllPossibleCodes( input.substr(2),output+ch2);
+        }
+    } 
+}
+void printAllPossibleCodes(string input)
+{
+    string output="";
+  printAllPossibleCodes( input,output);
+}
+
 
