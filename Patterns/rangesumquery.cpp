5209304d05c177211dc6da9539fee77f0f04e993
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-28 13:15:10 +0530

    Update rangesumquery.cpp

--- Patterns/rangesumquery.cpp
+++ Patterns/rangesumquery.cpp
@@ -1,3 +1,21 @@
+ NumArray(vector<int>& nums) {
+         v=nums;
+       for(int i=1;i<v.size();i++)
+         v[i]=v[i-1]+v[i];  
+    }
+    
+    int sumRange(int left, int right) {
+         if(left==0)
+           return v[right];
+        return v[right]-v[left-1];   
+    }
+
+
+
+
+
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MUTABLE<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 class NumArray {
     vector<int> v;
     vector<int> prefixSum;
