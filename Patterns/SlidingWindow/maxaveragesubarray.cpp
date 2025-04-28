f06ecbaf91229a0eac30b104c1ee814397c3b952
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-28 17:07:26 +0530

    Update maxaveragesubarray.cpp

--- Patterns/SlidingWindow/maxaveragesubarray.cpp
+++ Patterns/SlidingWindow/maxaveragesubarray.cpp
@@ -11,3 +11,21 @@ double findMaxAverage(vector<int>& nums, int k) {
         }
         return double(maxSum)/k;
     }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+          double findMaxAverage(vector<int>& nums, int k) {
+        int maxSum=INT_MIN;
+        int sum=0;
+       for(int i=0;i<k;i++)
+         sum+=nums[i];
+
+         maxSum=sum;
+
+        for(int i=k;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            sum-=nums[i-k];
+            if(sum>maxSum)
+             maxSum=sum; 
+        } 
+        return double(maxSum)/k;
+    }
