07b5948c57f27acfab6ab350e44d10186bfa9dc0
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-28 21:12:19 +0530

    Update HappyNumber.cpp

--- Patterns/FastAndSlowPtr/HappyNumber.cpp
+++ Patterns/FastAndSlowPtr/HappyNumber.cpp
@@ -21,3 +21,30 @@ int getNext(int n)
         } 
         return true; 
     }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    class Solution {
+public:
+    int getNext(int n)
+    {
+        int sum=0;
+        while(n>0)
+        {
+            int digit=n%10;
+            sum+=digit* digit;
+            n=n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+       int slow=n;
+       int fast=getNext(getNext(n));
+
+       while(fast!=1 && fast!=slow)
+       {
+        slow=getNext(slow);
+        fast=getNext(getNext(fast));
+       }
+
+       return fast==1;
+    }
+};
