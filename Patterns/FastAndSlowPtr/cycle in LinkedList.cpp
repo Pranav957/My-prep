ed4b2847929cf20e8a82b4cb4175614bc67451a7
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-28 20:40:07 +0530

    Update cycle in LinkedList.cpp

--- Patterns/FastAndSlowPtr/cycle in LinkedList.cpp
+++ Patterns/FastAndSlowPtr/cycle in LinkedList.cpp
@@ -11,3 +11,18 @@
         }
         return false;
     }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+ bool hasCycle(ListNode *head) {
+       ListNode* fast=head;
+       ListNode* slow=head;
+
+       while(fast!=NULL && fast->next!=NULL)
+       {
+          fast=fast->next->next;
+          slow=slow->next;
+          if(fast==slow)
+           return true;
+       }
+       return false;
+    }
