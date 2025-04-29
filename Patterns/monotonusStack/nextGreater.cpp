d360735efb485f060d92666db77bfcdf7cfb7a69
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-29 13:00:03 +0530

    Create nextGreater.cpp

---
+++ Patterns/monotonusStack/nextGreater.cpp
@@ -0,0 +1,25 @@
+vector<int> nextGreaterElements(vector<int>& nums) {
+       stack<int> s;
+       vector<int> v(nums.size(),-1);
+       for(int i=0;i<nums.size();i++)
+       {
+         while(!s.empty() && nums[i]>nums[s.top()])
+         {
+            int index=s.top();
+            s.pop();
+            v[index]=nums[i];
+         }
+         s.push(i);
+       }
+        for(int i=0;i<nums.size();i++)
+       {
+          while(nums[i]>nums[s.top()])
+          {
+            int index=s.top();
+            s.pop();
+            v[index]=nums[i];
+          }
+       } 
+      
+       return v;
+    }
