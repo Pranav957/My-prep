c6c9a67e29544d14b9a2eb4a47673a17c6f1511a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-04-30 12:49:15 +0530

    Update and rename kthlargestElement.cp to kthlargestElement.cpp

--- Patterns/topKelements/kthlargestElement.cpp
+++ Patterns/topKelements/kthlargestElement.cpp
@@ -16,3 +16,15 @@ int findKthLargest(vector<int>& nums, int k) {
         }
         return pq.top();
     }
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+
+        for(int i=0;i<nums.size();i++)
+        {
+           pq.push(nums[i]);
+           if(pq.size()>k)
+            pq.pop();
+        }
+        return pq.top();
+    }
