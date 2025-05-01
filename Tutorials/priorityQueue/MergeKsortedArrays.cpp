ad6fe139efdd3b89f8327e2dcf59cbde0a20ae71
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-01 10:27:12 +0530

    Update MergeKsortedArrays.cpp

--- Tutorials/priorityQueue/MergeKsortedArrays.cpp
+++ Tutorials/priorityQueue/MergeKsortedArrays.cpp
@@ -19,3 +19,51 @@ vector<int> mergeKSortedArrays(vector<vector<int>*> input) {
   }
   return v;
 }
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    #include <vector>
+#include <queue>
+using namespace std;
+
+// Define a struct to hold value along with array and element indices
+struct HeapNode {
+    int value;
+    int arrayIndex;
+    int elementIndex;
+
+    HeapNode(int val, int i, int j) : value(val), arrayIndex(i), elementIndex(j) {}
+
+    // Min-heap comparator
+    bool operator>(const HeapNode& other) const {
+        return value > other.value;
+    }
+};
+
+vector<int> mergeKSortedArrays(vector<vector<int>*>& input) {
+    // Min-heap (priority_queue with custom comparator)
+    priority_queue<HeapNode, vector<HeapNode>, greater<HeapNode>> minHeap;
+
+    // Push first element of each array into the heap
+    for (int i = 0; i < input.size(); i++) {
+        if (!input[i]->empty()) {
+            minHeap.emplace((*input[i])[0], i, 0);
+        }
+    }
+
+    vector<int> result;
+
+    // Extract min and push the next element from same array
+    while (!minHeap.empty()) {
+        HeapNode curr = minHeap.top();
+        minHeap.pop();
+        result.push_back(curr.value);
+
+        int nextIndex = curr.elementIndex + 1;
+        if (nextIndex < input[curr.arrayIndex]->size()) {
+            minHeap.emplace((*input[curr.arrayIndex])[nextIndex], curr.arrayIndex, nextIndex);
+        }
+    }
+
+    return result;
+}
+O(nlogk)
