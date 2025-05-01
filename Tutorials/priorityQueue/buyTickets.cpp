ab496827f75a0180866dad94a28ac7fe2adb4ec6
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-01 10:47:55 +0530

    Update buyTickets.cpp

--- Tutorials/priorityQueue/buyTickets.cpp
+++ Tutorials/priorityQueue/buyTickets.cpp
@@ -1,3 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int buyTicket(int *arr, int n, int k) {
+    // Write your code here
+    queue<int> elements;
+    priority_queue<int> pq;
+
+    for(int i=0;i<n;i++)
+    {
+        elements.push(i);
+        pq.push(arr[i]);
+    }
+    int time=0;
+
+    while(1)
+    {
+        int front=elements.front();
+        if(arr[front]==pq.top())
+        {
+            time++;
+            elements.pop();
+            pq.pop();
+            if(front==k)
+              return time;
+        }
+        else
+        {
+            elements.pop();
+            elements.push(front);
+        }
+        
+    }
+}
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
 #include<queue>
 int buyTicket(int *arr, int n, int k) {
     // Write your code here
