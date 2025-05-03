cad0dd00ba0c9a4157e7fa456880e4fc10a601af
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-03 16:38:11 +0530

    Create AdjcenctTraversal.cpp

---
+++ Tutorials/GraphTUT/AdjcenctTraversal.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void travelDfs(int v,vector<int> adj[],vector<int> &dfs, vector<bool>& visited,int sv)
+    {
+        dfs.push_back(sv);
+        visited[sv]=true;
+
+        for(auto neighbour: adj[sv])
+        {
+            if(!visited[neighbour])
+             travelDfs(v,adj,dfs,visited,neighbour);
+        }
+  
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+       vector<int> dfs;
+       vector<bool>visited(V);
+       travelDfs(V,adj,dfs,visited,0);
+        return dfs;
+
+    }
+
+int main()
+{
+    vector<int> adj[5]={{2,3,1},{0},{0,4},{2}};
+    
+    vector<int> res=dfsOfGraph(5,adj);
+    
+    for(int i=0;i<res.size();i++)
+      cout<<res[i]<<" ";
+    
+    std::cout<<"Hello World";
+
+    return 0;
+}
