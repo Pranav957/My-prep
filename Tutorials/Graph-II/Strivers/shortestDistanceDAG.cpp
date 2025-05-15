1ed865f71d8b7f383f9541430363ae4ea721027b
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-15 19:00:19 +0530

    Create shortestDistanceDAG.cpp

---
+++ Tutorials/Graph-II/Strivers/shortestDistanceDAG.cpp
@@ -0,0 +1,66 @@
+class Solution {
+    
+    private:
+      void trvelDFS(vector<pair<int,int>> adj[],int sv, vector<bool>& visited,stack<int>& st)
+      {
+          visited[sv]=true;
+          for(auto it : adj[sv])
+          {
+              if(!visited[it.first])
+              {
+                  trvelDFS(adj,it.first,visited,st);
+              }
+          }
+          
+          st.push(sv);
+      }
+  public:
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        // code here
+        vector<pair<int,int>> adj[V];
+        for(int i=0;i<E;i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int wt=edges[i][2];
+            adj[u].push_back({v,wt});
+        }
+        
+        vector<bool> visited(V);
+        
+        stack<int> st;
+        for(int i=0;i<V;i++)
+        {
+             if(!visited[i])
+               trvelDFS(adj,i,visited,st);
+        }
+        
+        vector<int> dist(V, INT_MAX);
+         
+        dist[0]=0;
+        
+        while(!st.empty())
+        {
+            int node=st.top();
+            st.pop();
+             if (dist[node] != INT_MAX) { // Only relax if node is reachable
+               for(auto it:adj[node])
+            {
+                int v=it.first;
+                int wt=it.second;
+                if(dist[v]>dist[node]+wt)
+                {
+                    dist[v]=dist[node]+wt;
+                }
+            }
+             }
+        }
+        for(int i=0;i<V;i++)
+         {
+             if(dist[i]==INT_MAX)
+              dist[i]=-1;
+         }
+         
+         return dist;
+          
+    }
