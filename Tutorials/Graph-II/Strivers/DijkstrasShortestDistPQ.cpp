9449e944019903c3e5eb3d978d7cbb3f53cf5a1a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-16 20:40:12 +0530

    Update DijkstrasShortestDistPQ.cpp

--- Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
+++ Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
@@ -37,3 +37,45 @@ class Solution {
        return dis;   
     }
 };
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<UsingSet<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<pair<int,int>> adj[V];
+        for(auto edge:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            int wt=edge[2];
+            adj[u].push_back({v,wt});
+        }
+        
+        vector<int> dis(V,INT_MAX);
+        dis[src]=0;
+        
+        set<pair<int,int>> st;
+        st.insert({0,src});
+        
+        while(!st.empty())
+        {
+            auto it=st.begin();
+            int node=it->second;
+            int dist=it->first;
+            st.erase(it);
+            
+            for(auto it:adj[node])
+            {
+                int adjNode=it.first;
+                int edgewt=it.second;
+                if(dist+edgewt<dis[adjNode])
+                {
+                    if(dis[adjNode]!=INT_MAX)
+                        st.erase({dis[adjNode],adjNode});
+                     dis[adjNode]=dist+edgewt;
+                     st.insert({dis[adjNode],adjNode});
+                }  
+            }
+        }
+       return dis;   
+    }
