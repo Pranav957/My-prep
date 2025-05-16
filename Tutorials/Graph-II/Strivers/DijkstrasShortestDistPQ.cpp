292000c5a2064d7b31cf3bae8babb8bd0c451877
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-16 20:02:08 +0530

    Create DijkstrasShortestDistPQ.cpp

---
+++ Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<pair<int,int>> adj[V];
+        for(auto edge:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            int wt=edge[2];
+            adj[u].push_back({v,wt});
+        }
+        
+        vector<int> dis(V,INT_MAX);
+        dis[src]=0;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,src});
+        
+        while(!pq.empty())
+        {
+            int node=pq.top().second;
+            int dist=pq.top().first;
+            pq.pop();
+            
+            for(auto it:adj[node])
+            {
+                int adjNode=it.first;
+                int edgewt=it.second;
+                if(dist+edgewt<dis[adjNode])
+                {
+                     dis[adjNode]=dist+edgewt;
+                     pq.push({dis[adjNode],adjNode});
+                }  
+            }
+        }
+       return dis;   
+    }
+};
