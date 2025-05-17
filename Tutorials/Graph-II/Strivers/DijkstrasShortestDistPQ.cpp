62e403a163a572756162fb0128f9963d358321db
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-17 15:40:07 +0530

    Update DijkstrasShortestDistPQ.cpp

--- Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
+++ Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
@@ -37,6 +37,46 @@ class Solution {
        return dis;   
     }
 };
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,UsingQueue<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<pair<int,int>> adj[V];
+        for(auto edge:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            int wt=edge[2];
+            adj[u].push_back({v,wt});
+        }
+        
+        vector<int> dis(V,INT_MAX);
+        dis[src]=0;
+        
+        queue<pair<int,int>> pq;
+        pq.push({0,src});
+        
+        while(!pq.empty())
+        {
+            int node=pq.front().second;
+            int dist=pq.front().first;
+            pq.pop();
+            
+            for(auto it:adj[node])
+            {
+                int adjNode=it.first;
+                int edgewt=it.second;
+                if(dist+edgewt<dis[adjNode])
+                {
+                     dis[adjNode]=dist+edgewt;
+                     pq.push({dis[adjNode],adjNode});
+                }  
+            }
+        }
+       return dis;   
+    }
+};
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<UsingSet<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
   class Solution {
   public:
