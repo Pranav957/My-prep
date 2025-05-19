b8d23e122347227f3ffa42c62f331720c4b47cf8
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-05-19 18:10:51 +0530

    Update DijkstrasShortestDistPQ.cpp

--- Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
+++ Tutorials/Graph-II/Strivers/DijkstrasShortestDistPQ.cpp
@@ -38,45 +38,46 @@ class Solution {
     }
 };
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,UsingQueue<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  class Solution {
-  public:
-    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        // Code here
-        vector<pair<int,int>> adj[V];
-        for(auto edge:edges)
-        {
-            int u=edge[0];
-            int v=edge[1];
-            int wt=edge[2];
-            adj[u].push_back({v,wt});
-        }
+  class Solution {                                                                   class Solution {
+  public:                                                                               public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {                    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        // Code here                                                                        // Code here
+        vector<pair<int,int>> adj[V];                                                       vector<pair<int,int>> adj[V];
+        for(auto edge:edges)                                                                 for(auto edge:edges)
+        {                                                                                     {
+            int u=edge[0];                                                                       int u=edge[0];
+            int v=edge[1];                                                                        int v=edge[1];
+            int wt=edge[2];                                                                       int wt=edge[2];  
+            adj[u].push_back({v,wt});                                                              adj[u].push_back({v,wt}); 
+        }                                                                                      }
+         
+        vector<int> dis(V,INT_MAX);                                                           vector<int> dis(V,INT_MAX);
+        dis[src]=0;                                                                            dis[src]=0;
         
-        vector<int> dis(V,INT_MAX);
-        dis[src]=0;
-        
-        queue<pair<int,int>> pq;
-        pq.push({0,src});
-        
-        while(!pq.empty())
-        {
-            int node=pq.front().second;
-            int dist=pq.front().first;
-            pq.pop();
-            
-            for(auto it:adj[node])
-            {
-                int adjNode=it.first;
-                int edgewt=it.second;
-                if(dist+edgewt<dis[adjNode])
-                {
-                     dis[adjNode]=dist+edgewt;
-                     pq.push({dis[adjNode],adjNode});
-                }  
-            }
-        }
-       return dis;   
-    }
-};
+        queue<pair<int,int>> pq;                                                             queue<int> pq;
+        pq.push({0,src});                                                                    pq.push(src);
+                                                                                              
+        while(!pq.empty())                                                                    while(!pq.empty())
+        {                                                                                    {
+            int node=pq.front().second;                                                         int node=pq.front();
+            int dist=pq.front().first;                                                          pq.pop();  
+            pq.pop();                                                                         
+                                                                                                 for(auto it:adj[node])
+            for(auto it:adj[node])                                                               {
+            {                                                                                       int adjNode=it.first;
+                int adjNode=it.first;                                                                int edgewt=it.second;
+                int edgewt=it.second;                                                               if(dis[node]+edgewt<dis[adjNode])                                   
+                if(dist+edgewt<dis[adjNode])                                                        {
+                {                                                                                     dis[adjNode]=dis[node]+edgewt;  
+                     dis[adjNode]=dist+edgewt;                                                          pq.push(adjNode);
+                     pq.push({dis[adjNode],adjNode});                                                } 
+                 }                                                                                 }  
+            }                                                                                   }
+        }                                                                                     return dis;
+       return dis;                                                                          } 
+    }                                                                                    };
+};                                                                                      
+
 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<UsingSet<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
   class Solution {
   public:
