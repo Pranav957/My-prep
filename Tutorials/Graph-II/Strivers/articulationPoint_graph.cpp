3ecf6fb3f09efd6c931e8e5ebfa1b3e698e30065
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-06-24 12:54:33 +0530

    Create articulationPoint_graph.cpp

---
+++ Tutorials/Graph-II/Strivers/articulationPoint_graph.cpp
@@ -0,0 +1,58 @@
+// User function Template for C++
+
+class Solution {
+    int timer=1;
+    void dfsTraversal(vector<int> adj[],int node,int parent,int tin[],int low[],vector<bool>& visited,vector<int>& mark)
+    {
+        visited[node]=true;
+        tin[node]=low[node]=timer;
+        timer++;
+        int child=0;
+        for(auto it:adj[node])
+        {
+            if(!visited[it])
+            {
+                dfsTraversal(adj,it,node,tin,low,visited,mark);
+                low[node]=min(low[node],low[it]);
+                if(low[it]>=tin[node] && parent!=-1)   // node should reach before not the parent node
+                {
+                    mark[node]=1;
+                }
+                child++;
+            }
+            else{
+                low[node]=min(low[node],tin[it]);
+            }
+        }
+        if(child>1 && parent==-1)
+           mark[node]=1;
+          
+    }
+  public:
+    vector<int> articulationPoints(int V, vector<int> adj[]) {
+        // Code here
+        int n=V;
+        vector<bool> visited(n,false);
+        int tin[n];
+        int low[n];
+        vector<int> mark(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i])
+              dfsTraversal(adj,i,-1,tin,low,visited,mark);
+        }
+        
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(mark[i]==1)
+              ans.push_back(i);
+        }
+        
+        if(ans.size()==0)
+          return {-1};
+        
+        return ans;  
+    }
+    
+};
