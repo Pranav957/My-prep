d7fb43a932a0ed31831e4a663bd6493eaa2f4455
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-01 17:44:07 +0530

    Create frogJump.cpp

---
+++ Tutorials/dpTutorial/1DDp/frogJump.cpp
@@ -0,0 +1,56 @@
+class Solution {
+  public:
+     int minCost(vector<int>& height, int n, vector<int>& dp) {
+        if (n <= 0)
+            return 0;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int a = abs(height[n ] - height[n - 1]) + minCost(height, n - 1, dp);
+        int b = INT_MAX;
+          if(n>=2)
+            b = abs(height[n ] - height[n - 2]) + minCost(height, n - 2, dp);
+
+        return dp[n] = min(a, b);
+    }
+
+    int minCost(vector<int>& height) {
+        int n = height.size();
+        vector<int> dp(n + 1, -1);
+        // return minCost(height, n-1, dp);
+        dp[0]=0;
+        
+        for(int i=1;i<n;i++)
+        {
+            int a=abs(height[i] - height[i - 1])+dp[i-1];
+            int b=INT_MAX;
+            if(i>=2)
+             b=abs(height[i] - height[i - 2])+dp[i-2];
+            
+            dp[i]=min(a,b); 
+        }
+        return dp[n-1];
+    }
+};
+//  with k solution
+// class Solution {
+// public:
+//     int frogJump(vector<int>& heights, int k,int n) {
+//         if(n<=0)
+//          return 0;
+   
+//     int mn=INT_MAX;
+//      for(int i=1;i<k;i++)
+//       if(n-i>=1)
+//       {
+//          int a= frogJump(heights,k,n-i)+abs(heights[n-1]-heights[n-i-1]);
+//          mn=min(a,mn);
+//       }
+//       return mn;
+//     }
+   
+//      int frogJump(vector<int>& heights, int k){
+//         return frogJump(heights,k,heights.size());
+//      }
+// };
