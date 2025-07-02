0fed9356564e622705809e4c52ff8a5231b8a488
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-02 19:48:29 +0530

    Update EditDistance.cpp

--- Tutorials/dpTutorial/1DDp/DPOnStrings/EditDistance.cpp
+++ Tutorials/dpTutorial/1DDp/DPOnStrings/EditDistance.cpp
@@ -68,3 +68,48 @@ int minDistance(string word1, string word2,int m,int n,int** dp){
         return dp[m][n];  
     }
 };
+
+**********************************************************************************Space Ob***************************************************************************************88
+   int minDistance(string word1, string word2) {
+        int m=word1.length();
+        int n=word2.length();
+        
+
+        // int** dp=new int*[m+1];
+        // for(int i=0;i<m+1;i++)
+        // {
+        //     dp[i]=new int[n+1];
+        // }
+
+        // for(int i=0;i<m+1;i++)
+        // {
+        //     dp[i][0]=i;
+        // }
+        // for(int j=0;j<n+1;j++)
+        //   dp[0][j]=j;
+        vector<int> prev(n+1,0),curr(n+1,0);
+        for(int j=0;j<n+1;j++)
+        {
+            prev[j]=j;
+        }
+
+        for(int i=1;i<m+1;i++)
+        {
+            curr[0]=i;
+            for(int j=1;j<n+1;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                  curr[j]=prev[j-1];
+
+                 else
+                 {
+                    int insertop=curr[j-1];
+                    int deleteOp=prev[j];
+                    int replaceOp=prev[j-1];
+                    curr[j]=1+min({insertop,deleteOp,replaceOp});
+                 }  
+            }
+             prev=curr; 
+        }
+        return prev[n];  
+    }
