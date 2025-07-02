ba3dbea6646eff44958dcfb75ce8637144cfd548
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-02 15:46:22 +0530

    Create longestCommonSustring.cpp

---
+++ Tutorials/dpTutorial/1DDp/DPOnStrings/longestCommonSustring.cpp
@@ -0,0 +1,38 @@
+ int longestCommonSubstr(string text1, string text2) {
+        // int m = s1.length(), n = s2.length();
+        // vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(max(m, n) + 1, -1)));
+        // return solve(s1, s2, m, n, 0, dp);
+        int m=text1.length();
+        int n=text2.length();
+
+        int** dp=new int*[m+1];
+        for(int i=0;i<=m;i++)
+        {
+            dp[i]=new int[n+1];
+        }
+
+        for(int i=0;i<m+1;i++)
+        {
+            dp[i][0]=0;
+        }
+        for(int j=0;j<n+1;j++)
+          dp[0][j]=0;
+          
+          int ans=0;
+
+          for(int i=1;i<m+1;i++)
+          {
+            for(int j=1;j<n+1;j++)
+               if(text1[i-1]==text2[j-1])
+              {
+                dp[i][j]=1+dp[i-1][j-1];
+                ans=max(ans,dp[i][j]);
+              }     
+              else
+              {
+                dp[i][j]=0;
+              }  
+          }
+
+          return ans;
+    }
