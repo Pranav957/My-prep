3752937387b2a5f8c721631d1b8d5c83cc125d4d
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-02 11:38:33 +0530

    Update LCS.cpp

--- Tutorials/dpTutorial/1DDp/DPOnStrings/LCS.cpp
+++ Tutorials/dpTutorial/1DDp/DPOnStrings/LCS.cpp
@@ -56,3 +56,26 @@ int longestCommonSubsequence(string text1, string text2,int m, int n,int** dp) {
           return dp[m][n];
     }
 };
+
+************************************************************************************************************************************
+int longestCommonSubsequence(string text1, string text2) {
+         int m=text1.length();
+        int n=text2.length();
+
+        vector<int> prev(n+1,0);
+        vector<int> curr(n+1,0);
+
+          for(int i=1;i<m+1;i++)
+          {
+            for(int j=1;j<n+1;j++)
+               if(text1[i-1]==text2[j-1])
+                 curr[j]=1+prev[j-1];
+               else
+               {
+                curr[j]=max(curr[j-1],prev[j]);
+               }
+               prev=curr;  
+          }
+
+          return prev[n];
+    }
