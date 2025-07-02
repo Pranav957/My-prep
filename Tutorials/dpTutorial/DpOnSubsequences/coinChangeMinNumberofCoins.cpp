8182bc1b045642b8978866f2afa80eeeba6f4374
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-02 10:16:01 +0530

    Update coinChangeMinNumberofCoins.cpp

--- Tutorials/dpTutorial/DpOnSubsequences/coinChangeMinNumberofCoins.cpp
+++ Tutorials/dpTutorial/DpOnSubsequences/coinChangeMinNumberofCoins.cpp
@@ -51,3 +51,72 @@ public:
         int res= dp[n][amount];
        return (res == INT_MAX-1  ) ? -1 : res;
      }
+
+*****************************************************************************************************************************************************************************************
+   int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        // vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        // int res= coinChange(coins,coins.size(),amount,dp);
+        //  return (res == INT_MAX - 1) ? -1 : res;
+
+        // for(int i=0;i<n+1;i++)
+        // {
+        //     for(int j=0;j<amount+1;j++)
+        //     {
+        //         if(j==0)
+        //           dp[i][j]=0;
+        //         else if(i==0)
+        //           dp[i][j]=INT_MAX-1;   
+        //     }
+        // }
+        vector<int> prev(amount+1,INT_MAX-1);
+        vector<int> curr(amount+1,0);
+
+        // for(int j=0;j<amount+1;j++)
+        //   prev[j]=INT_MAX-1;
+
+         prev[0]=0; 
+        
+        for(int i=1;i<n+1;i++)
+        {
+            curr[0]=0;
+            for(int j=1;j<amount+1;j++)
+            {
+                if(coins[i-1]<=j)
+                 curr[j]=min(1+curr[j-coins[i-1]],prev[j]);
+                else
+                curr[j]=prev[j];
+            }
+           prev=curr; 
+        }
+        int res= prev[amount];
+       return (res == INT_MAX-1  ) ? -1 : res;
+     }
+
+    //  int coinChange(vector<int>& coins, int amount) {
+    //     int n = coins.size();
+    //     vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+
+    //     for(int i = 0; i <= n; i++) {
+    //         for(int j = 0; j <= amount; j++) {
+    //             if(j == 0)
+    //                 dp[i][j] = 0;
+    //             else if(i == 0)
+    //                 dp[i][j] = INT_MAX - 1;
+    //         }
+    //     }
+
+    //     for(int i = 1; i <= n; i++) {
+    //         for(int j = 1; j <= amount; j++) {
+    //             if(coins[i - 1] <= j)
+    //                 dp[i][j] = min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
+    //             else
+    //                 dp[i][j] = dp[i - 1][j];
+    //         }
+    //     }
+
+    //     int res = dp[n][amount];
+    //     return (res == INT_MAX - 1) ? -1 : res;
+    // }
+    
+};
