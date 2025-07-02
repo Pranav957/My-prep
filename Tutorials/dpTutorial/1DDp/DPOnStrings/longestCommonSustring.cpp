242728e47225f62ef4c3e0702b9f00036054a11c
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-02 16:00:07 +0530

    Update longestCommonSustring.cpp

--- Tutorials/dpTutorial/1DDp/DPOnStrings/longestCommonSustring.cpp
+++ Tutorials/dpTutorial/1DDp/DPOnStrings/longestCommonSustring.cpp
@@ -1,4 +1,24 @@
- int longestCommonSubstr(string text1, string text2) {
+int solve(string &s1, string &s2, int i, int j) {
+        if(i == 0 || j == 0) return 0;
+
+
+        int ans= solve(s1, s2, i - 1, j - 1);
+        
+        if(s1[i-1]==s2[j-1])
+         ans=ans+1;
+         
+         return ans;
+         
+    }
+    int longestCommonSubstr(string& s1, string& s2) {
+        // your code here
+        int n=s1.length();
+        int m=s2.length();
+        return solve(s1,s2,n,m);
+    }
+
+
+int longestCommonSubstr(string text1, string text2) {
         // int m = s1.length(), n = s2.length();
         // vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(n + 1, vector<int>(max(m, n) + 1, -1)));
         // return solve(s1, s2, m, n, 0, dp);
