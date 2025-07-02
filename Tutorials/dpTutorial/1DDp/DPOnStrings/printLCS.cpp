8805dac90d4af490e1f5bc3d92242674570d0e6f
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-02 13:20:28 +0530

    Update printLCS.cpp

--- Tutorials/dpTutorial/1DDp/DPOnStrings/printLCS.cpp
+++ Tutorials/dpTutorial/1DDp/DPOnStrings/printLCS.cpp
@@ -1,3 +1,60 @@
+ string longestCommonSubsequence(string text1, string text2) {
+         int m=text1.length();
+        int n=text2.length();
+
+        int** dp=new int*[m+1];
+        for(int i=0;i<=m;i++)
+        {
+            dp[i]=new int[n+1];
+        }
+
+        for(int i=0;i<m+1;i++)
+        {
+            dp[i][0]=0;
+        }
+        for(int j=0;j<n+1;j++)
+          dp[0][j]=0;
+
+          for(int i=1;i<m+1;i++)
+          {
+            for(int j=1;j<n+1;j++)
+               if(text1[i-1]==text2[j-1])
+                 dp[i][j]=1+dp[i-1][j-1];
+               else
+               {
+                dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+               }  
+          }
+
+		  int i=m; int j=n;
+		  string s="";
+		  while(i>0 && j>0)
+		  {
+			  if(text1[i-1]==text2[j-1])
+               {
+				   s.push_back(text1[i-1]);
+				   i--;j--;
+			   } 
+			   else{
+				   if(dp[i][j-1]>dp[i-1][j])
+				     j--;
+				   else
+				    i--;	 
+			   }
+		  }
+		  reverse(s.begin(),s.end());
+  return s;
+
+}
+string findLCS(int n, int m,string &s1, string &s2){
+	// Write your code here.
+   return  longestCommonSubsequence(s1,s2);
+}
+**********************************************************************************************************************************************************************
+
+
+
+
 class Solution {
     priority_queue<pair<int,string>, vector<pair<int,string>>, greater<pair<int,string>>> pq;
     set<string> seen;
