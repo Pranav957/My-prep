37a1b225c41c2a0a3e909c16038f3f6b6fef4ac5
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-03 16:12:42 +0530

    Update countLIS.cpp

--- Tutorials/dpTutorial/DPOnLIS/countLIS.cpp
+++ Tutorials/dpTutorial/DPOnLIS/countLIS.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    int lengthOfLIS(vector<int>& nums,int prev,int idx,int n,vector<vector<int>>& dp) { //LIS starting from ith index with previous x
+        if(idx==n)
+         return 0;
+        
+        if(dp[prev+1][idx]!=-1)
+         return dp[prev+1][idx];
+
+         int b=0;
+         if(prev==-1 || nums[idx]>nums[prev])
+          b= 1+lengthOfLIS(nums,idx,idx+1,n,dp);
+
+          int a= lengthOfLIS(nums,prev,idx+1,n,dp);
+
+       return dp[prev+1][idx]= max(a,b);    
+    }
     int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+       return lengthOfLIS(nums,-1,0,nums.size(),dp);
+    }
+};
+
+***************************************************************************************************************
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {  // LIS ending at ith index;
         int n=nums.size();
         vector<int> ans(n,1);
      
