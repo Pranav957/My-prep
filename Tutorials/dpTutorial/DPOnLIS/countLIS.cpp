0b10e22f6314e858a1faa13101a3935ca871cf81
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-03 17:05:47 +0530

    Update countLIS.cpp

--- Tutorials/dpTutorial/DPOnLIS/countLIS.cpp
+++ Tutorials/dpTutorial/DPOnLIS/countLIS.cpp
@@ -79,6 +79,24 @@ public:
 
 ***************************************************************************************************************
 
+     int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+     vector<int> dp(n,1);
+     int maxi=1;
+     for(int i=0;i<n;i++)
+     {
+        for(int prev=0;prev<i;prev++)
+        {
+            if(nums[i]>nums[prev])
+             dp[i]=max(dp[i],1+dp[prev]);
+        }
+        maxi=max(maxi,dp[i]);
+     }
+     return maxi;
+        
+    }
+*********************************************************************************************************************************
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {  // LIS ending at ith index;
