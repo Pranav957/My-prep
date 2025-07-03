22fadf2fd972939903f8d065a1df1aa57622eee3
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-03 12:55:02 +0530

    Update stocks3.cpp

--- Tutorials/dpTutorial/DpOnStocks/stocks3.cpp
+++ Tutorials/dpTutorial/DpOnStocks/stocks3.cpp
@@ -63,3 +63,30 @@ public:
     }
 
 };
+
+***************************************************************************************************************************************************************
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> after(2,vector<int>(3,0));
+         vector<vector<int>> curr(2,vector<int>(3,0));
+
+
+         for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= 1; j++) {
+            for (int k = 1; k <= 2; k++) {  // skip k == 0
+                if (j == 1) {
+                    // buy
+                    curr[j][k] = max(-prices[i] + after[0][k], after[1][k]);
+                } else {
+                    // sell
+                    curr[j][k] = max(prices[i] + after[1][k - 1], after[0][k]);
+                }
+            }
+        }
+        after=curr;
+    }
+         
+
+         return after[1][2];
+    }
+
