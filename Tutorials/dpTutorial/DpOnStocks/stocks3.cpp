f620a03273cb2f0600b5ec9d6d9fee41932d753c
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-03 12:45:10 +0530

    Create stocks3.cpp

---
+++ Tutorials/dpTutorial/DpOnStocks/stocks3.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+     int maxProfit(vector<int>& prices,int idx,bool buy,int n,int trans, vector<vector<vector<int>>>& dp) {
+        if(idx==n || trans==0)
+          return 0;
+
+          if(dp[idx][buy][trans]!=-1)
+           return dp[idx][buy][trans];
+
+        if(buy)
+        {
+            int a= (-prices[idx]+maxProfit(prices,idx+1,false,n,trans,dp)); 
+            int b=maxProfit(prices,idx+1,true,n,trans,dp); 
+            return dp[idx][buy][trans]=max(a,b);
+        } 
+        else
+        {
+            int a= prices[idx]+maxProfit(prices,idx+1,true,n,trans-1,dp); 
+            int b=maxProfit(prices,idx+1,false,n,trans,dp); 
+             return dp[idx][buy][trans]= max(a,b);
+        }
+        
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        // return maxProfit(prices,0,true,prices.size(),2,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+
+        //  for(int i=0;i<=n;i++)  //already initialized to 0
+        //  {
+        //     for(int j=0;j<2;j++)
+        //     {
+        //         dp[i][j][0]=0;
+        //     }
+        //  }
+
+        //  for(int j=0;j<2;j++)
+        //  {
+        //     for(int k=0;k<3;k++)
+        //     {
+        //         dp[n][j][k]=0;
+        //     }
+        //  }
+
+
+         for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= 1; j++) {
+            for (int k = 1; k <= 2; k++) {  // skip k == 0
+                if (j == 1) {
+                    // buy
+                    dp[i][j][k] = max(-prices[i] + dp[i + 1][0][k], dp[i + 1][1][k]);
+                } else {
+                    // sell
+                    dp[i][j][k] = max(prices[i] + dp[i + 1][1][k - 1], dp[i + 1][0][k]);
+                }
+            }
+        }
+    }
+         
+
+         return dp[0][1][2];
+    }
+
+};
