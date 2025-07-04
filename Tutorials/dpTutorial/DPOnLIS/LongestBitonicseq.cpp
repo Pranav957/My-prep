00b1ab9751a80eeabbfa515706a3935501f29d7d
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-04 15:07:44 +0530

    Update LongestBitonicseq.cpp

--- Tutorials/dpTutorial/DPOnLIS/LongestBitonicseq.cpp
+++ Tutorials/dpTutorial/DPOnLIS/LongestBitonicseq.cpp
@@ -40,3 +40,41 @@ class Solution {
 }
     
 };
+**************************************************************************
+ int LongestBitonicSequence(int n, vector<int> &nums) {
+        // code here
+
+     vector<int> dp1(n,1);
+     for(int i=0;i<n;i++)
+     {
+        for(int prev=0;prev<i;prev++)
+        {
+            if(nums[i]>nums[prev])
+             dp1[i]=max(dp1[i],1+dp1[prev]);
+        }
+     }
+    
+ 
+     vector<int> dp2(n,1);
+
+     for(int i=n-1;i>=0;i--)
+     {
+        for(int prev=n-1;prev>i;prev--)
+        {
+            if(nums[i]>nums[prev])
+             dp2[i]=max(dp2[i],1+dp2[prev]);
+        }
+     }
+
+    
+    
+    int maxi = 0;
+
+    // Find the maximum length of the bitonic subsequence
+    for (int i = 0; i < n; i++) {
+        if(dp1[i]>1 && dp2[i]>1)
+          maxi = max(maxi, dp1[i] + dp2[i] - 1);
+    }
+
+    return maxi;
+}
