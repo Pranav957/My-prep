075026513402c2d7946e4eabedca271110769ccb
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-04 18:46:25 +0530

    Update matrixChain.cpp

--- Tutorials/dpTutorial/PartitionDp/matrixChain.cpp
+++ Tutorials/dpTutorial/PartitionDp/matrixChain.cpp
@@ -29,3 +29,30 @@ class Solution {
    return matrixMultiplication(arr,1,n-1,dp);
     }
 };
+******************************************************************************
+int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+//   return matrixMultiplication(arr,1,n-1,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        
+        for(int i=0;i<n;i++)
+         dp[i][i]=0;       //as 0 no need to write;
+         
+         for(int i=n-1;i>=1;i--)
+         {
+             for(int j=i+1;j<=n-1;j++)
+             {    
+                 int mini=INT_MAX;
+                   for(int k=i;k<j;k++)
+                     {
+                         int ans=arr[i-1]*arr[k]*arr[j]+
+                         dp[i][k]+dp[k+1][j];
+                         mini=min(mini,ans);
+                     }
+                 dp[i][j]=mini;     
+             }
+         }
+         return dp[1][n-1]; //opposite to top down
+    }
