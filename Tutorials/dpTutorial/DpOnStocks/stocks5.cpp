9024ef86de6735a05b0012a36b1237f2146241c9
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-07 22:25:46 +0530

    Create stocks5.cpp

---
+++ Tutorials/dpTutorial/DpOnStocks/stocks5.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+  int maxProfit(vector<int>& prices,int idx,bool buy,int n, vector<vector<int>>& dp,int fee) {
+        if(idx>=n)
+         return 0;
+
+         if(dp[idx][buy]!=-1)
+          return dp[idx][buy];
+
+        if(buy)
+        {
+            int a=-prices[idx]+maxProfit(prices,idx+1,false,n,dp,fee);
+            int b=maxProfit(prices,idx+1,true,n,dp,fee);
+            return dp[idx][buy]= max(a,b);
+        } 
+        else
+        {
+            int a=prices[idx]+maxProfit(prices,idx+1,true,n,dp,fee)-fee;
+            int b=maxProfit(prices,idx+1,false,n,dp,fee);
+            return dp[idx][buy]= max(a,b);
+        }
+
+    }
+     int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        // vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        // return maxProfit(prices,0,true,n,dp,fee);
+        vector<vector<int>>dp(n+2,vector<int>(2,0));
+
+         for(int i=n-1;i>=0;i--)
+         {
+            for(int j=0;j<2;j++)
+            {
+                if(j)
+                {
+                    int a=-prices[i]+dp[i+1][0];
+                    int b=dp[i+1][1];
+                    dp[i][j]=max(a,b);
+                }
+                else
+                {
+                    int a=prices[i]-fee+dp[i+1][1];
+                    int b=dp[i+1][0];
+                    dp[i][j]=max(a,b);
+                }
+            }
+         }
+
+         return dp[0][true];
+    }
+
+};
