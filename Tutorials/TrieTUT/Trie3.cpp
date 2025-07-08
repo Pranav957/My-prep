bf58abdab6e6d79637511021a81b0b2938dcf987
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-08 22:29:50 +0530

    Create Trie3.cpp

---
+++ Tutorials/TrieTUT/Trie3.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h> 
+ class TrieNode{
+    TrieNode* childrens[26];
+    int countEnd;
+    int countprefix;
+
+    public:
+     TrieNode()
+     {
+        countEnd=0;
+        countprefix=0;
+        for(int i=0;i<26;i++)
+          childrens[i]=NULL;
+     }
+
+     bool containsKey(char ch)
+     {
+        return childrens[ch-'a']!=NULL;
+     }
+
+     TrieNode* get(char ch)
+     {
+        return childrens[ch-'a'];
+     }
+
+     void put(char ch,TrieNode* node)
+     {
+        childrens[ch-'a']=node;
+     }
+
+     void increaseEnd()
+     {
+         countEnd++;
+     }
+
+     void increasePrefix()
+     {
+         countprefix++;
+     }
+     void reduceEnd()
+     {
+         countEnd--;
+     }
+     void reducePrefix()
+     {
+         countprefix--;
+     }
+
+     int getEnd(){
+         return countEnd;
+     }
+
+     int getPrefix(){
+         return countprefix;
+     }
+     
+};
+class Trie{
+     TrieNode* root;
+    public:
+
+    Trie(){
+        // Write your code here.
+        root=new TrieNode();
+    }
+
+    void insert(string &word){
+        // Write your code here
+        TrieNode* node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(!node->containsKey(word[i]))
+            {
+                node->put(word[i],new TrieNode());
+            }
+            node=node->get(word[i]);
+            node->increasePrefix();
+        }
+        node->increaseEnd();
+    }
+
+    int countWordsEqualTo(string &word){
+        // Write your code here.
+        TrieNode* node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(!node->containsKey(word[i]))
+              return 0;
+            node=node->get(word[i]);  
+        }
+        return node->getEnd();
+    }
+
+    int countWordsStartingWith(string &word){
+        // Write your code here.
+        TrieNode* node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(!node->containsKey(word[i]))
+              return 0;
+            node=node->get(word[i]);  
+        }
+        return node->getPrefix();
+    }
+
+    void erase(string &word){
+        // Write your code here.
+        TrieNode* node=root;
+        for(int i=0;i<word.size();i++)
+        {
+            if(node->containsKey(word[i]))
+            {
+                node=node->get(word[i]);
+                node->reducePrefix();
+            }else
+             return;
+        }
+        node->reduceEnd();
+    }
+};
