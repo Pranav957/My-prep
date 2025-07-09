da960948b04c8816168c724ea3b2475ca7e48b33
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-09 19:01:10 +0530

    Update countDistinctSubstrings.cpp

--- Tutorials/TrieTUT/countDistinctSubstrings.cpp
+++ Tutorials/TrieTUT/countDistinctSubstrings.cpp
@@ -68,3 +68,30 @@ int countDistinctSubstring(string str)
     // Return size of the HashSet
     return result.size()+1;
 }
+******************************************************************************************************************************************888
+    int countDistinctSubstring(string str)
+{
+    TrieNode* head = new TrieNode();
+
+    // will hold the count of unique substrings
+    int count = 0;
+    // included count of substr " "
+
+    for (int i = 0; i < str.length(); i++) {
+        TrieNode* temp = head;
+
+        for (int j = i; j < str.length(); j++) {
+            // when char not present add it to the trie
+            if (temp->child[str[j] - 'a'] == NULL) {
+                temp->child[str[j] - 'a'] = new TrieNode();
+                temp->isWord = 1;
+                count++;
+            }
+            // move on to the next char
+            temp = temp->child[str[j] - 'a'];
+        }
+    }
+
+    return count;
+}
+
