c6d9088ad389539168153149745be91d2930d1b1
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-15 10:42:46 +0530

    Create JumpGame.cpp

---
+++ Patterns/greedy/JumpGame.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        if(nums.size()==1)
+         return true;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+             {
+                int x=2;
+                int j;
+                for( j=i-1;j>=0;j--)
+                {
+                   if(nums[j]>=x)
+                    break;
+
+                    if(i==n-1 && nums[j]>=x-1)
+                    break;
+                    
+
+                    x++;
+                }
+                if(j==-1)
+                 return false;
+             } 
+        }
+        return true;
+    }
+};
