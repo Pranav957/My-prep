5d918e3cae109ded9b0b2acc5dfd34dad461253e
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-15 10:45:32 +0530

    Update JumpGame.cpp

--- Patterns/greedy/JumpGame.cpp
+++ Patterns/greedy/JumpGame.cpp
@@ -28,3 +28,15 @@ public:
         return true;
     }
 };
+******************************************************************************************************************************************
+bool canJump(vector<int>& nums) {
+        int maxInd=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i>maxInd)
+             return false;
+
+             maxInd=max(maxInd,i+nums[i]);
+        }
+        return true;
+    }
