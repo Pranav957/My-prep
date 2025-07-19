b32afcedf1b459c67af4be8876881e9b7184f541
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-19 21:31:31 +0530

    Update lemondateChange.cpp

--- Patterns/greedy/lemondateChange.cpp
+++ Patterns/greedy/lemondateChange.cpp
@@ -29,3 +29,30 @@ public:
         return true;
     }
 };
+********************************************************************************************************************************************************************88
+    class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        map<int,int> mp;
+
+        for(int i=0;i<bills.size();i++)
+        {
+            int remaining=bills[i]-5;
+            if(remaining > 0 && mp.empty())
+              return false;
+            
+            for(auto it=mp.rbegin(); it!=mp.rend();it++)
+            {
+                while(remaining!=0 && remaining>=it->first && it->second>0 )
+                {
+                    remaining-=it->first;
+                    it->second--;
+                }
+            }
+            if(remaining>0)return false;
+                    
+            mp[bills[i]]++;
+        }
+        return true;
+    }
+};
