130e71d5b1bb8636a0b23a32b8d19da787a44249
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-19 21:17:35 +0530

    Create assignCookies.cpp

---
+++ Patterns/greedy/assignCookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+       int i=0,j=0;
+       while(i<s.size())
+       {
+          if(s[i]>=g[j])
+           j++;
+
+           if(j==g.size())
+            break;
+          i++; 
+       }
+       return j;
+        
+    }
+};
