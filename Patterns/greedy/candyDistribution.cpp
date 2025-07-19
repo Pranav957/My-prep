70184684ff8dcafccf79224cca73a7ad5f3c948d
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-20 00:45:40 +0530

    Update candyDistribution.cpp

--- Patterns/greedy/candyDistribution.cpp
+++ Patterns/greedy/candyDistribution.cpp
@@ -1,31 +1,3 @@
-class Solution {  dp problems can be solved by greedy with using range
-public:
-    int jump(vector<int>& nums) {
-        int n=nums.size();
-
-        int jumps=0;
-        int l=0,r=0;
-        while(r<n-1)
-        {
-            int farthest=0;
-            for(int i=l;i<=r;i++)
-            {
-                farthest=max(farthest,i+nums[i]);
-            }
-            l=r+1;
-            r=farthest;
-            jumps+=1;
-
-        }
-
-        
-        return jumps;
-    }
-    
-};
-
-********************************************************************************************************************************************************************************
-
 
 
 int candy(vector<int>& ratings) {
