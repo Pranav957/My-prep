485b23a7ebb87f12b9f922e0de0722eb741f520d
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-20 23:00:46 +0530

    Update FruitsIntoBasket.cpp

--- Patterns/SlidingWindow/strivers/FruitsIntoBasket.cpp
+++ Patterns/SlidingWindow/strivers/FruitsIntoBasket.cpp
@@ -21,3 +21,22 @@ public:
     }
 };
 *********************************************************************************************************************
+int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        unordered_set<int> st;
+        int total=0;
+      for(int i=0;i<n;i++)
+      {
+        for(int j=i;j<n;j++)
+        {
+          st.insert(fruits[j]);  
+          if(st.size()<=2)
+           total=max(total,j-i+1);
+          else
+           break;  
+        }
+        st.clear();
+        
+      }
+      return total;
+    }
