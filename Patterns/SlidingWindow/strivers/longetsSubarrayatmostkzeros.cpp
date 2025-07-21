ef71b0a39e449a36cae700bcbd69a9949b66d02b
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-21 17:48:27 +0530

    Update longetsSubarrayatmostkzeros.cpp

--- Patterns/SlidingWindow/strivers/longetsSubarrayatmostkzeros.cpp
+++ Patterns/SlidingWindow/strivers/longetsSubarrayatmostkzeros.cpp
@@ -45,3 +45,28 @@ public:
         return longest;
     }
 };
+*************************************************************************************************************************88
+      int longestOnes(vector<int>& nums, int k) {
+        int longest=0;
+        int left=0;
+        int zeros=0;
+        for(int r=0;r<nums.size();r++)
+        {
+          if(nums[r]==0)
+          {
+             zeros++;
+          }
+          if(zeros<=k)
+          {
+             longest=max(longest,r-left+1);
+          }
+          else
+            {
+                if(nums[left]==0)
+                 zeros--;  
+               left++;
+               
+            }
+        }
+        return longest;
+    }
