b9ab54c85d34e57f8b1e7f6fa4c4bbe36042d72a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-26 14:09:16 +0530

    Update LongestSubstringWitKuniqueChar.cpp

--- Patterns/SlidingWindow/strivers/LongestSubstringWitKuniqueChar.cpp
+++ Patterns/SlidingWindow/strivers/LongestSubstringWitKuniqueChar.cpp
@@ -27,3 +27,28 @@ class Solution {
         
     }
 };
+*************************************************************************
+class Solution {
+  public:
+    int longestKSubstr(string &s, int k) {
+        // code here
+        int n=s.length(),mx=-1;
+        unordered_set<char> st;
+        for(int i=0;i<s.length();i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+               st.insert(s[j]);
+               if(st.size()>k)
+                 break;
+               if(st.size()==k)     
+                  mx=max(mx,j-i+1);         
+            }
+            st.clear();
+            
+        }
+        return mx;
+        
+        
+    }
+};
