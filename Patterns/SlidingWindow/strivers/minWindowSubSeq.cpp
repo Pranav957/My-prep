fbb302adf4ac72d727fd8445493eac5891a4883a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-26 21:52:28 +0530

    Update minWindowSubSeq.cpp

--- Patterns/SlidingWindow/strivers/minWindowSubSeq.cpp
+++ Patterns/SlidingWindow/strivers/minWindowSubSeq.cpp
@@ -27,3 +27,48 @@ string minWindow(string s, string T)
 	}
 	return ans;
 }
+******************************************************************************
+string minWindow(string s, string T)
+{
+	// Write your Code here
+	int n=s.length(),m=T.length();
+	string ans="";
+    int minLength=INT_MAX;
+	int startIndex=-1;
+
+   int left=0,x=0;
+   for(int r=0;r<n;r++)
+   {
+	   if(s[r]==T[x])
+	   {
+		   if(x==0)
+		     left=r;
+		     x++;
+
+		  if(x==m)
+		  {
+			//   startIndex=left;
+			//   minLength=min(minLength,r-left+1);
+			  if(r-left+1<minLength)
+			  {
+				  minLength=r-left+1;
+				  startIndex=left;
+			  }
+
+			  left++;
+			  while(s[left]!=T[1])
+			   {
+				   if(s[left]==T[0])
+				   {
+					   minLength=min(minLength,r-left+1);
+					   startIndex=left;
+				   } 
+			       left++;		
+			   }
+			   x=0;
+		  } 	 
+	   }
+   }
+	
+	return (startIndex==-1)? "":s.substr(startIndex,minLength);
+}
