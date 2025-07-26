1c7255f6c2e0b93db49929643e1a0dcd9aa7eff1
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-26 18:00:40 +0530

    Update minWindowSubstr.cpp

--- Patterns/SlidingWindow/strivers/minWindowSubstr.cpp
+++ Patterns/SlidingWindow/strivers/minWindowSubstr.cpp
@@ -31,3 +31,37 @@ public:
          return ans;
     }
 };
+******************************************************************************************8
+    class Solution {
+public:
+    string minWindow(string s, string t) {
+        //  unordered_map<char,int> mp;
+          int n=s.length();
+          int minLength=INT_MAX;
+          int startIndx=-1;
+
+         for(int i=0;i<n;i++)
+         { 
+            int cnt=0;
+            int mp[256]={0};
+            for(int i=0;i<t.length();i++)
+               mp[t[i]]++;
+            for(int j=i;j<n;j++)
+            {
+                if (mp[s[j]] > 0) 
+                cnt++;
+                mp[s[j]]--;
+                if(cnt==t.length())
+                {
+                   if(j-i+1<minLength)
+                   {
+                    minLength=j-i+1;
+                    startIndx=i;
+                   } 
+                    break;
+                } 
+            }
+         } 
+         return (startIndx == -1) ? "" : s.substr(startIndx, minLength);
+    }
+};
