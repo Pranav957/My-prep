80c26f0ee9775cbe736c622fecc64e30807010bc
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-28 11:23:10 +0530

    Create handOfStraignts.cpp

---
+++ Patterns/topKelements/strivers/handOfStraignts.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        
+        map<int,int> mp;
+
+        if(hand.size()%groupSize!=0)
+         return false;
+
+         for(int i=0;i<hand.size();i++)
+         {
+            mp[hand[i]]++;
+         }
+
+          while(!mp.empty())
+         {
+            auto it=mp.begin();
+            int curr=it->first;
+
+            for(int j=0;j<groupSize;j++)
+            {
+              if(mp[curr+j]<1)
+               return false;
+
+               mp[curr+j]--;
+
+               if(mp[curr+j]<1)
+                mp.erase(curr+j);
+            } 
+         }
+         return true;
+        
+        
+
+    }
+};
