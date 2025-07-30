cfbad4951e49e71c738cdc2f70dcc57f5ca83c47
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-30 21:35:31 +0530

    Update TaskSchedular.cpp

--- Patterns/topKelements/strivers/TaskSchedular.cpp
+++ Patterns/topKelements/strivers/TaskSchedular.cpp
@@ -1,1 +1,47 @@
 
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> mp(26,0);
+        for(auto& task: tasks)
+        {
+            mp[task-'A']++;
+        }
+       
+       int time=0;
+       priority_queue<int> pq;
+
+       for(int i=0;i<26;i++)
+       {
+          if(mp[i]>0)
+            pq.push(mp[i]);
+       }
+
+       while(!pq.empty())
+       {
+        vector<int> temp;
+          for(int i=1;i<=n+1;i++)
+          {
+            if(!pq.empty())
+            {
+                int ele=pq.top();
+                pq.pop();
+                ele--;
+            temp.push_back(ele);
+            }
+          }
+
+          for(auto &f:temp)
+          {
+            if(f>0)
+             pq.push(f);
+          }
+          
+          if(pq.empty())
+             time+=temp.size();
+          else
+             time+=n+1;  
+       }
+     return time;
+    }
+};
