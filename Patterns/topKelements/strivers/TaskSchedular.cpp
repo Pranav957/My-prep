50946ebc14dbe38ae5f71dccff5d0ce7d2a38c2e
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-07-31 11:58:22 +0530

    Update TaskSchedular.cpp

--- Patterns/topKelements/strivers/TaskSchedular.cpp
+++ Patterns/topKelements/strivers/TaskSchedular.cpp
@@ -1,4 +1,31 @@
 
+
+
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> freq(26,0);
+        for(int i=0;i<tasks.size();i++)
+        {
+            freq[tasks[i]-'A']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>());
+    
+        int ideals=(freq[0]-1)*n;
+
+        for(int i=1;i<freq.size();i++)
+        {
+           
+            ideals=ideals-min(freq[0]-1,freq[i]);
+        }
+
+        return tasks.size()+max(0,ideals);
+
+    }
+};
+
+*******************************************************************************
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
