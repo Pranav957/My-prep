30802f1be297d3176da0503efa33e7eded3b60de
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-01 15:53:52 +0530

    Update mergeKsortedArray.cpp

--- Patterns/topKelements/strivers/mergeKsortedArray.cpp
+++ Patterns/topKelements/strivers/mergeKsortedArray.cpp
@@ -1,32 +1,33 @@
 class Solution {
   public:
-    // Function to merge k sorted arrays.
-//     vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
-//         // code here
-//       priority_queue< 
-//     pair<int, pair<int, int>>, 
-//     vector<pair<int, pair<int, int>>>, 
-//     greater<pair<int, pair<int, int>>> 
-// > pq;
-//         for(int i=0;i<arr.size();i++)
-//         {
-//             pq.push({arr[i][0],{i,0}});
-//         }
-//         vector<int> ans;
+    Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
+        // code here
+      priority_queue< 
+    pair<int, pair<int, int>>, 
+    vector<pair<int, pair<int, int>>>, 
+    greater<pair<int, pair<int, int>>> 
+> pq;
+        for(int i=0;i<arr.size();i++)
+        {
+            pq.push({arr[i][0],{i,0}});
+        }
+        vector<int> ans;
         
-//         while(!pq.empty())
-//         {
-//             auto top = pq.top();
-//             int val = top.first;
-//             int arrIdx = top.second.first;
-//             int eleIdx = top.second.second;
-//             pq.pop();
-//             ans.push_back(val);
-//             if(eleIdx+1<arr[arrIdx].size())
-//               pq.push({arr[arrIdx][eleIdx+1],{arrIdx,eleIdx+1}});
-//         }
-//         return ans;
-//     }
+        while(!pq.empty())
+        {
+            auto top = pq.top();
+            int val = top.first;
+            int arrIdx = top.second.first;
+            int eleIdx = top.second.second;
+            pq.pop();
+            ans.push_back(val);
+            if(eleIdx+1<arr[arrIdx].size())
+              pq.push({arr[arrIdx][eleIdx+1],{arrIdx,eleIdx+1}});
+        }
+        return ans;
+    }
+*********************************************************************************************************************
   vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
         priority_queue< 
             pair<int, pair<int, int>>, 
