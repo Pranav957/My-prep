6b80627a2b28e05c295d5328e49e75129109d199
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-03 17:26:49 +0530

    Update uniqueSubsets.cpp

--- Patterns/Recursion/striver/uniqueSubsets.cpp
+++ Patterns/Recursion/striver/uniqueSubsets.cpp
@@ -40,3 +40,23 @@ public:
         return ans;
     }
 };
+*************************************************************************************************************
+vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        // vector<int>v;
+        // subsets(nums,v,0,ans);
+        // return ans;
+        int n=nums.size();
+        for(int num=0;num<pow(2,n);num++)//  for(int num=0;num<(1<<n);num++)
+        {
+            vector<int> v;
+            for(int i=0;i<n;i++)
+            {
+              if((num&(1<<i))!=0)  // if((num&(1<<i))
+                v.push_back(nums[i]);
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+*************************************************************************************************
