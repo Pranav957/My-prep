87183147e142d92752a2e0cf428ef45d95f6bc55
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-05 22:31:21 +0530

    Update postFixToInfix.cpp

--- Patterns/striver/StackAndQues/postFixToInfix.cpp
+++ Patterns/striver/StackAndQues/postFixToInfix.cpp
@@ -22,3 +22,28 @@ class Solution {
         return st.top();
     }
 };
+**********************************************************PREFIXTOINFIX***********************************************************
+  // User function Template for C++
+
+class Solution {
+  public:
+    string preToInfix(string exp) {
+        // Write your code here
+         int i=exp.length()-1;
+        stack<string> st;
+        while(i>=0)
+        {
+           if((exp[i]>='A' && exp[i]<='Z')|| (exp[i]>='a' && exp[i]<='z')||(exp[i]>='0' && exp[i]<='9'))
+            st.push(string(1,exp[i]));
+           else
+           {
+               string t1=st.top();st.pop();
+               string t2=st.top();st.pop();
+               string ans="("+t1+exp[i]+t2+")";
+               st.push(ans);
+           }
+           i--;
+        }
+        return st.top();
+    }
+};
