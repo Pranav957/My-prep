3139a64253981b04dfbd6042d27b05ffe0e460e6
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-06 12:10:03 +0530

    Create nextGreater.cpp

---
+++ Patterns/striver/StackAndQues/nextGreater.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int x=*max_element(nums2.begin(),nums2.end());
+        vector<int> mp(x+1,-1);
+        stack<int> st;
+        for(int i=0;i<nums2.size();i++)
+        {
+          while(!st.empty()&& nums2[i]>st.top())
+          {
+            mp[st.top()]=nums2[i];
+            st.pop();
+          }
+          st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int i=0;i<nums1.size();i++)
+        {
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
+    }
+};
