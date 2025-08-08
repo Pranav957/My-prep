41532d7d1d875170189da6183098b380133acaef
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-08 12:35:40 +0530

    Create RemoveKDigits.cpp

---
+++ Patterns/striver/StackAndQues/RemoveKDigits.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string removeKdigits(string nums, int k) { //get rid of larger ones and keep smaller ones at start
+
+        // if(k==nums.length())
+        //   return "0";
+        stack<char> st;
+        st.push(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            while(!st.empty() && nums[i]<st.top() && k>0)
+            {
+                st.pop();
+                k--;
+            }
+            st.push(nums[i]);
+        }
+        
+        while(k>0 && !st.empty())
+       {
+         st.pop();
+         k--;
+       } 
+
+       string str="";
+       while(!st.empty())
+       {
+        str+=st.top();
+        st.pop();
+       }
+
+       reverse(str.begin(),str.end());  // cpp consider string as part of stl;
+       int i=0;
+       while(!str.empty() && str[0]=='0')
+        {
+            str=str.substr(1);
+            i++;
+        } 
+
+        if(str=="")
+          return "0";
+
+     return str;
+         
+    }
+};
