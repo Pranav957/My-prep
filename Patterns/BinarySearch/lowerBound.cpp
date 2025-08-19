f86739c92b1cc8549c461bfc702c0aea5be96691
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-19 20:52:20 +0530

    Create lowerBound.cpp

---
+++ Patterns/BinarySearch/lowerBound.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+     int lowerBound(vector<int>& arr, int target,int low,int high,int n)
+     {
+         int ans=n;
+         
+         while(low<=high)
+         {
+             int mid=(low+high)/2;
+             
+             if(arr[mid]>=target)
+             {
+                 ans=mid;
+                 high=mid-1;
+             }
+             else
+              low=mid+1;
+         }
+         return ans;
+     }
+    int lowerBound(vector<int>& arr, int target) {
+        // code here
+        return lowerBound(arr,target,0,arr.size()-1,arr.size());
+    }
+};
