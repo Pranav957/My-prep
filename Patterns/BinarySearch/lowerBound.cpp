3ffdf1e92f9eb9e834dab1f8d7b37242875b0ef0
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-19 21:30:22 +0530

    Update lowerBound.cpp

--- Patterns/BinarySearch/lowerBound.cpp
+++ Patterns/BinarySearch/lowerBound.cpp
@@ -27,3 +27,29 @@ class Solution {
        return ind;
     }
 };
+***********************************************************************************************************************
+class Solution {
+  public:
+   int upperBound(vector<int>& arr, int target,int low,int high,int n)
+     {
+         int ans=n;
+         
+         while(low<=high)
+         {
+             int mid=(low+high)/2;
+             
+             if(arr[mid]>target)
+             {
+                 ans=mid;
+                 high=mid-1;
+             }
+             else
+              low=mid+1;
+         }
+         return ans;
+     }
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        return upperBound(arr,target,0,arr.size()-1,arr.size());
+    }
+};
