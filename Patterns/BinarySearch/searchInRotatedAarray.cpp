13b970cefc019fe727ccdf2eeb76650f55b09a1d
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-20 14:51:31 +0530

    Update searchInRotatedAarray.cpp

--- Patterns/BinarySearch/searchInRotatedAarray.cpp
+++ Patterns/BinarySearch/searchInRotatedAarray.cpp
@@ -29,3 +29,37 @@ public:
         return helper(nums,0,nums.size()-1,target);
     }
 };
+***********************************************************
+class Solution {
+public:
+   int helper(vector<int>& nums,int left,int right,int target)
+   {
+      while(left<=right)
+      {
+        int mid=(left+right)/2;
+
+        if(nums[mid]==target)
+         return mid;
+
+        if(nums[left]<=nums[mid])
+        {
+           if(target>=nums[left] && target<nums[mid])
+              right=mid-1;
+           else
+            left=mid+1;
+
+        }else
+        {
+            if(target>nums[mid] && target<=nums[right])
+              left=mid+1;
+           else
+           right=mid-1; 
+        } 
+      }
+      return -1;
+       
+   }
+    int search(vector<int>& nums, int target) {
+        return helper(nums,0,nums.size()-1,target);
+    }
+};
