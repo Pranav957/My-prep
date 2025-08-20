3d27c451746bc3331e09f3d4ea94d3ac328edf1a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-20 14:41:01 +0530

    Create searchInRotatedAarray.cpp

---
+++ Patterns/BinarySearch/searchInRotatedAarray.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   int helper(vector<int>& nums,int left,int right,int target)
+   {
+      if(left>right)
+       return -1;
+
+       int mid=(left+right)/2;
+
+       if(nums[mid]==target)
+         return mid;
+
+       if(nums[left]<=nums[mid])
+       {
+          if(target>=nums[left] && target<=nums[mid])
+             return helper(nums,left,mid-1,target);
+          else
+           return helper(nums,mid+1,right,target);
+       }
+       else
+       {
+        if(target>=nums[mid] && target<=nums[right])
+            return helper(nums,mid+1,right,target);
+          else
+            return helper(nums,left,mid-1,target);
+       }
+   }
+    int search(vector<int>& nums, int target) {
+        return helper(nums,0,nums.size()-1,target);
+    }
+};
