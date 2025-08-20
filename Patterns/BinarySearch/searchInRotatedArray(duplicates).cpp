9d35cd90aff63a92f956a31bf2d605209b92e343
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-20 15:25:37 +0530

    Update searchInRotatedArray(duplicates).cpp

--- Patterns/BinarySearch/searchInRotatedArray(duplicates).cpp
+++ Patterns/BinarySearch/searchInRotatedArray(duplicates).cpp
@@ -38,3 +38,40 @@ public:
         return helper(nums,0,nums.size()-1,target);
     }
 };
+*******************************************************************************************************************************8
+     class Solution {
+public:
+   bool helper(vector<int>& nums,int left,int right,int target)
+   {
+      if(left>right)
+       return false;
+
+       int mid=(left+right)/2;
+
+       if(nums[mid]==target)
+         return true;
+
+        if(nums[left]==nums[mid] && nums[mid]==nums[right])
+        {
+          return helper(nums,left+1,right-1,target);
+        }  
+
+       if(nums[left]<=nums[mid])
+       {
+          if(target>=nums[left] && target<=nums[mid])
+             return helper(nums,left,mid-1,target);
+          else
+           return helper(nums,mid+1,right,target);
+       }
+       else
+       {
+        if(target>=nums[mid] && target<=nums[right])
+            return helper(nums,mid+1,right,target);
+          else
+            return helper(nums,left,mid-1,target);
+       }
+   }
+    bool search(vector<int>& nums, int target) {
+        return helper(nums,0,nums.size()-1,target);
+    }
+};
