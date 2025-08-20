137cea886180eff543983ec7098f6a5f82763c28
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-20 15:22:56 +0530

    Create searchInRotatedArray(duplicates).cpp

---
+++ Patterns/BinarySearch/searchInRotatedArray(duplicates).cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+     bool helper(vector<int>& nums,int left,int right,int target) //O(n/2) shrinking might required
+   {
+      while(left<=right)
+      {
+        int mid=(left+right)/2;
+
+        if(nums[mid]==target)
+         return true;
+
+          if(nums[left]==nums[mid] && nums[mid]==nums[right])
+        {
+            left=left+1;
+            right=right-1;
+           continue;
+        }  
+
+        if(nums[left]<=nums[mid])
+        {
+           if(target>=nums[left] && target<nums[mid])
+              right=mid-1;
+           else
+            left=mid+1;
+
+        }else
+        {
+            if(target>nums[mid] && target<=nums[right])
+              left=mid+1;
+           else
+           right=mid-1; 
+        } 
+      }
+      return false;
+   }
+      
+    bool search(vector<int>& nums, int target) {
+        return helper(nums,0,nums.size()-1,target);
+    }
+};
