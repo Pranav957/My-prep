3a0cad60d03ec3d7de61fded5b12896e39522000
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-20 17:12:20 +0530

    Update mininrotatedSortedArray.cpp

--- Patterns/BinarySearch/mininrotatedSortedArray.cpp
+++ Patterns/BinarySearch/mininrotatedSortedArray.cpp
@@ -21,3 +21,34 @@ public:
         return ans;
     }
 };
+************************************************************************************************
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low=0,high=nums.size()-1;
+        int ans=INT_MAX;
+        while(low<=high)
+        {
+           if(nums[low]<=nums[high])  //slight obtimization
+           {
+            ans=min(nums[low],ans);
+            break;
+           }
+
+            int mid=(low+high)/2;
+
+
+            if(nums[low]<=nums[mid])
+            {
+                ans=min(ans,nums[low]);
+                low=mid+1;
+            }
+            else
+            {
+                ans=min(ans,nums[mid]);
+                 high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
