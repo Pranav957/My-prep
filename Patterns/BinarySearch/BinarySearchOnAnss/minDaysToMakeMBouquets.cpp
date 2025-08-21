a90f0808d8ff6a103abbe3c8c2b1b4f01adfdd1e
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-21 21:20:48 +0530

    Update minDaysToMakeMBouquets.cpp

--- Patterns/BinarySearch/BinarySearchOnAnss/minDaysToMakeMBouquets.cpp
+++ Patterns/BinarySearch/BinarySearchOnAnss/minDaysToMakeMBouquets.cpp
@@ -41,6 +41,25 @@ public:
         return ans;
     }
 };
+******************************************************************************************
+  int minDays(vector<int>& bloomDay, int m, int k) {
+        if(bloomDay.size()<(long long)m*k)
+         return -1;
+        int low=*min_element(bloomDay.begin(),bloomDay.end());
+        int high=*max_element(bloomDay.begin(),bloomDay.end());
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(isPossible(bloomDay,m,k,mid))
+            {
+                high=mid-1;
+            }
+            else
+             low=mid+1;
+        }
+        return low;
+    }
 ******************************************************************************************************************************************
 class Solution {
 public:
