33c1c25be8906922eeca8fb00ab05fa383ba5f32
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-22 13:15:49 +0530

    Create bookAllocation.cpp

---
+++ Patterns/BinarySearch/BinarySearchOnAnss/minOfMax-MaxOfMin/bookAllocation.cpp
@@ -0,0 +1,73 @@
+class Solution {
+  public:
+    int findAloocation(vector<int>& arr, int pages)
+    {
+        int stu, pagesAll=0;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(pagesAll+arr[i]<=pages)
+            {
+                pagesAll+=arr[i];
+            }
+            else{
+                stu++;
+                pagesAll=arr[i];
+            }
+        }
+        return stu;
+    }
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        int low=*max_element(arr.begin(),arr.end());
+        int high=accumulate(arr.begin(),arr.end(),0);
+        
+        for(int pages=low;pages<=high;pages++)
+        {
+             int cntSt=findAloocation(arr,pages);
+             if(cntSt==k)return pages;
+        }
+    }
+};
+******************************************************************************************************************
+class Solution {
+  public:
+    int findAloocation(vector<int>& arr, int pages)
+    {
+        int stu=1, pagesAll=0;
+        
+        for(int i=0;i<arr.size();i++)
+        {
+            if(pagesAll+arr[i]<=pages)
+            {
+                pagesAll+=arr[i];
+            }
+            else{
+                stu++;
+                pagesAll=arr[i];
+            }
+        }
+        return stu;
+    }
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        if (arr.size() < k) return -1;
+        int low=*max_element(arr.begin(),arr.end());
+        int high=accumulate(arr.begin(),arr.end(),0);
+        int ans=-1;
+       while(low<=high)
+        {
+            int mid=(low+high)/2;
+            
+             int cntSt=findAloocation(arr,mid);
+             if(cntSt<=k)
+             {
+                high=mid-1; 
+                ans=mid;
+             }
+             else
+              low=mid+1;
+        }
+        return ans; //return low
+    }
+};
