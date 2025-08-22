563c56ee68ce018de74b6d5c260ed3f23d0b2758
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-22 12:27:31 +0530

    Update agrresiveCows.cpp

--- Patterns/BinarySearch/BinarySearchOnAnss/minOfMax-MaxOfMin/agrresiveCows.cpp
+++ Patterns/BinarySearch/BinarySearchOnAnss/minOfMax-MaxOfMin/agrresiveCows.cpp
@@ -34,3 +34,47 @@ class Solution {
          }
     }
 };
+************************************************************************************************************
+class Solution {
+  public:
+    bool isPossible(vector<int>& stalls,int k,int dist)
+    {
+        int cntCows=1,last=stalls[0];
+        
+        for(int i=1;i<stalls.size();i++)
+        {
+            if(stalls[i]-last>=dist)
+            {
+                last=stalls[i];
+                cntCows++;
+            }
+        }
+        
+        if(cntCows>=k) return true;
+        return false;
+        
+    }
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        int maxi=*max_element(stalls.begin(),stalls.end());
+        int mini=*min_element(stalls.begin(),stalls.end());
+         int mDist=maxi-mini;
+         
+         sort(stalls.begin(),stalls.end());
+         
+         int low=1,high=mDist,ans=-1;
+         
+         while(low<=high)
+         {
+             int mid=(low+high)/2;
+             if(isPossible(stalls,k,mid))
+             {
+                 ans=mid;
+                 low=mid+1;
+             }
+             else
+              high=mid-1;
+         }
+         return high;
+    }
+};
