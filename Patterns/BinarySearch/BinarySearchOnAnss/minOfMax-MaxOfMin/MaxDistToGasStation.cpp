37e6f079985e15d061b476f14d6e9878a4da5c13
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-22 17:32:11 +0530

    Update MaxDistToGasStation.cpp

--- Patterns/BinarySearch/BinarySearchOnAnss/minOfMax-MaxOfMin/MaxDistToGasStation.cpp
+++ Patterns/BinarySearch/BinarySearchOnAnss/minOfMax-MaxOfMin/MaxDistToGasStation.cpp
@@ -31,3 +31,27 @@ class Solution {
         return maxAns;
     }
 };
+*****************************************************************************************************************
+class Solution {
+  public:
+    double findSmallestMaxDist(vector<int> &stations, int k) {
+        // Code here
+        int n=stations.size();
+        vector<int> howMany(n-1,0);
+        priority_queue<pair<long double,int>> pq;
+        for(int i=0;i<n-1;i++)
+        {
+           pq.push({arr[i+1]-arr[i],i});  
+        }
+        for(int gasStations=1;gasStations<=k;gasStations++)
+        {
+           auto tp=pq.top();po.pop();
+           int secIndex=tp.second;
+           howMany[secIndex]++;
+           long double initDiff=stations[secIndex+1]-stations[secIndex];
+           long double newSecLen=initDiff/(long double)(howMany[secInex]+1);
+           pq.push({newSecLen, secIndex});
+        }
+        return pq.top().first;
+    }
+};
