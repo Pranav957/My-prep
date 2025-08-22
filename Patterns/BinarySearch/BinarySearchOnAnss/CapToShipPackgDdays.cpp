7706f29ce756248d537129507523691306a3c21e
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-22 10:47:06 +0530

    Update CapToShipPackgDdays.cpp

--- Patterns/BinarySearch/BinarySearchOnAnss/CapToShipPackgDdays.cpp
+++ Patterns/BinarySearch/BinarySearchOnAnss/CapToShipPackgDdays.cpp
@@ -73,3 +73,39 @@ public:
         return ans;
     }
 };
+********************************************************************************************************************
+class Solution {
+public:
+   int getDays(vector<int>& weights,int cap)
+   {
+    int sum=0,cnt=0;
+     for(int i=0;i<weights.size();i++)
+     {
+        if(sum+weights[i]>cap)
+        {
+            sum=0;
+            cnt++;
+        }
+       
+        sum+=weights[i];
+     }
+     return cnt+1;
+   }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int high=accumulate(weights.begin(),weights.end(),0);
+        int low=*max_element(weights.begin(),weights.end());
+
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(getDays(weights,mid)<=days)
+            {
+                high=mid-1;
+            }
+            else
+              low=mid+1;
+        }
+        return low;
+    }
+};
