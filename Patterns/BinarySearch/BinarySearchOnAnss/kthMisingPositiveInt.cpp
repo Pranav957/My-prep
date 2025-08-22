9883d06ad6ad52f8ac9cfcfb37c1c5389c32fde1
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-22 11:46:04 +0530

    Update kthMisingPositiveInt.cpp

--- Patterns/BinarySearch/BinarySearchOnAnss/kthMisingPositiveInt.cpp
+++ Patterns/BinarySearch/BinarySearchOnAnss/kthMisingPositiveInt.cpp
@@ -20,3 +20,25 @@ public:
         
     }
 };
+***********************************************************************************************************************
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+      int low=0,high=arr.size()-1;
+
+      while(low<=high)
+      {
+        int mid=(low+high)/2;
+        int missing=arr[mid]-(mid+1);
+
+        if(missing<k)
+         low=mid+1;
+        else
+         high=mid-1;  
+      } 
+      return high+1+k; 
+    }
+};
+//ans= arr[high]+more;
+//more= k-missing
+// missing= arr[high]-(high+1); 
