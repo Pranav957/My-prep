7406703ee782c1346792b4d34229da979d39ea6f
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-23 12:01:30 +0530

    Update SearchIn@Darray.cpp

--- Patterns/BinarySearch/BSOn2DArrays/SearchIn@Darray.cpp
+++ Patterns/BinarySearch/BSOn2DArrays/SearchIn@Darray.cpp
@@ -21,3 +21,32 @@ public:
         return false;
     }
 };
+*****************************************************************************************************************************
+class Solution {
+    int binarySearch(vector<int>& input, int element,int i,int j)  
+{
+    while(i<=j)
+    {
+      int mid=(i+j)/2;
+        if(element==input[mid])
+         return mid;
+         if(element<input[mid])
+          {
+            j=mid-1;
+          }  
+          else
+           i=mid+1;
+    }
+    return -1;
+}
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size(),n=matrix[0].size();
+        for(int i=0;i<m;i++)
+        {
+            if(target>= matrix[i][0] && target<=matrix[i][n-1])
+              return binarySearch(matrix[i],target,0,n-1)!=-1;
+        }
+        return false;
+    }
+};
