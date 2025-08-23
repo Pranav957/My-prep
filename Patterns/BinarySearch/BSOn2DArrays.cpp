5eb20e9bbf369ad6bde70a58ab2f0c2a363e25eb
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-23 11:25:05 +0530

    Create BSOn2DArrays.cpp

---
+++ Patterns/BinarySearch/BSOn2DArrays.cpp
@@ -0,0 +1,25 @@
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
+        int m=arr.size(),n=arr[0].size();
+         int maxi=0, row=-1;
+       for(int i=0;i<m;i++)
+       {
+           int cnt=0;
+           for(int j=0;j<n;j++)
+           {
+              if(arr[i][j]==1)
+               cnt++;
+           }
+          if(cnt>maxi)
+          {
+              maxi=cnt;
+              row=i;
+          }
+       }
+       return row;
+        
+    }
+};
