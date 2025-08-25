10849be3da8b58f37f165f901aab3a2343d6613c
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-25 13:00:47 +0530

    Update checkIfRotationOfOther.cpp

--- Patterns/Strings/checkIfRotationOfOther.cpp
+++ Patterns/Strings/checkIfRotationOfOther.cpp
@@ -14,3 +14,23 @@ public:
     }
    
 };
+*********************************************************ANAGRAM**********************************8
+    class Solution {
+public:
+     bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false; // lengths must match
+
+        unordered_map<char,int> mp;
+
+        for (char c : s) {
+            mp[c]++;
+        }
+
+        for (char c : t) {
+            mp[c]--;
+            if (mp[c] == 0) mp.erase(c);
+        }
+
+        return mp.empty();
+    }
+};
