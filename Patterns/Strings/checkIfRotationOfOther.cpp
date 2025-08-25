418299760f27fa4ca805562a2e57075d822d27d2
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-25 17:02:59 +0530

    Update checkIfRotationOfOther.cpp

--- Patterns/Strings/checkIfRotationOfOther.cpp
+++ Patterns/Strings/checkIfRotationOfOther.cpp
@@ -34,3 +34,22 @@ public:
         return mp.empty();
     }
 };
+*************************************************************MAX Depth*********************************************
+    class Solution {
+public:
+    int maxDepth(string s) {
+        int maxi=0;
+        int cnt=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+              cnt++;
+            else if(s[i]==')')
+             cnt--;
+
+             maxi=max(maxi,cnt);  
+        }
+        return maxi;
+    }
+};
