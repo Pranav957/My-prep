b726b46dda489fa239bb8a7da96f46ddb98e98a5
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-28 12:39:18 +0530

    Update intersectionBetweentwo.cpp

--- Tutorials/LinkedListTUT/Striver/intersectionBetweentwo.cpp
+++ Tutorials/LinkedListTUT/Striver/intersectionBetweentwo.cpp
@@ -25,6 +25,60 @@ public:
 Better Approach-> toavoid space complexity 
   count the length of both the linked lists and find d as (n2-n1)
   now travel large list d step so that bot comes to common point
+    start treversing untill t1!=t2
+
+
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+   ListNode* collistion(ListNode* t1 , ListNode* t2, int d)
+   {
+      while(d)
+      {
+        t2=t2->next;
+        d--;
+      }
+
+      while(t1!=t2)
+      {
+        t1=t1->next;
+        t2=t2->next;
+      }
+      return t1;
+   }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+        ListNode* t1=headA,*t2=headB;
+        int a=0,b=0;
+        while(t1!=NULL)
+        {
+          t1=t1->next;
+          a++;
+        }
+        while(t2!=NULL)
+        {
+          t2=t2->next;
+          b++;
+        }
+
+         t1 = headA;
+        t2 = headB;
+
+        if(a<b)
+        {
+            return collistion(t1,t2,b-a);
+        }
+        else
+         return collistion(t2, t1,a-b);
+    }
+};
   ***************************************************************
    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
          if(!headA || !headB)return NULL;
