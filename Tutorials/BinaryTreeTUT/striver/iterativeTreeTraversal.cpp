a007ffbae527fbc31d50eb069b88274168ce7c3d
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-08-29 12:48:32 +0530

    Update iterativeTreeTraversal.cpp

--- Tutorials/BinaryTreeTUT/striver/iterativeTreeTraversal.cpp
+++ Tutorials/BinaryTreeTUT/striver/iterativeTreeTraversal.cpp
@@ -52,3 +52,75 @@ public:
         return inorder;
     }
 };
+*******************************************************
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+
+    if(root == NULL)return postorder;
+    stack<TreeNode*> st1, st2;
+    st1.push(root);
+
+    while(!st1.empty()){
+        root = st1.top();
+        st1.pop();
+
+        st2.push(root);
+
+        if(root->left != NULL){
+            st1.push(root->left);
+        }
+
+        if(root->right != NULL){
+            st1.push(root->right);
+        }
+    }
+
+    while(!st2.empty()){
+        postorder.push_back(st2.top()->val);
+        st2.pop();
+    }    
+    return postorder;
+
+    }
+};
+**********************************************************************allTraversals*******************
+    vector<vector<int>> preInPostTraversal(Node* root) {
+
+    vector<int> pre, in, post;
+    if (root == NULL) {
+        return {};
+    }
+    stack<pair<Node*, int>> st;
+    st.push({root, 1});
+    while (!st.empty()) {
+        auto it = st.top();
+        st.pop();
+        if (it.second == 1) {
+            pre.push_back(it.first->data);
+            it.second = 2;
+            st.push(it); 
+            if (it.first->left != NULL) {
+                st.push({it.first->left, 1});
+            }
+        }
+        else if (it.second == 2) {
+            in.push_back(it.first->data);
+            it.second = 3;
+            st.push(it); 
+            if (it.first->right != NULL) {
+                st.push({it.first->right, 1});
+            }
+        }
+        else {
+            post.push_back(it.first->data);
+        }
+    }
+
+    vector<vector<int>> result;
+    result.push_back(pre);
+    result.push_back(in);
+    result.push_back(post);
+    return result;
+}
