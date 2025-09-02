7047aa226dd494934fd1fe5307fc5f3ce9ac7756
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-02 22:16:18 +0530

    Create isSymmetric.cpp

---
+++ Tutorials/BinaryTreeTUT/striver/isSymmetric.cpp
@@ -0,0 +1,14 @@
+bool isSymmetric(TreeNode* left,TreeNode* right)
+    {
+        if(left==NULL || right==NULL)
+          return left==right;
+
+          if(left->val!=right->val)
+           return false;
+
+          return isSymmetric(left->left,right->right)&&
+           isSymmetric(left->right,right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return (root==NULL || isSymmetric(root->left,root->right));
+    }
