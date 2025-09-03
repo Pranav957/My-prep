6402e83c5cbb8d5325d881148163c250b74c6730
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-03 21:36:44 +0530

    Update childrensumInBinaryTree.cpp

--- Tutorials/BinaryTreeTUT/striver/HardProbs/childrensumInBinaryTree.cpp
+++ Tutorials/BinaryTreeTUT/striver/HardProbs/childrensumInBinaryTree.cpp
@@ -15,3 +15,28 @@ class Solution {
     return isSumProperty(root->left) && isSumProperty(root->right);
 }
 };
+********************************************maintainChildSumProperty*******************************************************
+  void changeTree(BinaryTreeNode < int > * root) {
+    
+   
+    if(root==NULL) return ;
+    int x=root->left?root->left->data:0;
+    int y=root->right?root->right->data:0;
+
+    
+   if(root->data>x+y)
+   {
+     if(root->left)root->left->data=root->data;
+     if(root->right)root->right->data=root->data;  
+   }
+    
+
+     changeTree(root->left);
+     changeTree(root->right);
+
+    int val=0;
+    if(root->left)val+=root->left->data;
+    if(root->right)val+=root->right->data;
+    if(root->left || root->right)root->data=val;
+    
+}
