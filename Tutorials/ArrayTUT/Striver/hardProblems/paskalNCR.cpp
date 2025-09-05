e6f9810b68b61cfba35d21a90bfbbaa4667f3add
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-05 22:03:51 +0530

    Update paskalNCR.cpp

--- Tutorials/ArrayTUT/Striver/hardProblems/paskalNCR.cpp
+++ Tutorials/ArrayTUT/Striver/hardProblems/paskalNCR.cpp
@@ -8,3 +8,41 @@ int findNCR(int n,int r)
      }
      return res;
   }
+*******************************printNthRow**********************************
+  int findNCR(int n)
+  {
+     int ans;
+     cout<< ans;
+     for(int i=1;i<n;i++)
+     {
+        ans=ans*(n-i);
+        ans=ans/i;
+        cout<<ans<<" ";
+     }
+  }
+**************************************************************************print Triangle***************************
+  class Solution {
+public:
+  vector<int> generateRow(int n)
+  {
+    long long ans=1;
+     vector<int> ansRow;
+    ansRow.push_back(ans);
+
+     for(int i=1;i<n;i++)
+     {
+        ans=ans*(n-i);
+        ans=ans/i;
+        ansRow.push_back(ans);
+     }
+     return ansRow ;
+  }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1;i<=numRows;i++)
+        {
+            ans.push_back(generateRow(i));
+        }
+        return ans;
+    }
+};
