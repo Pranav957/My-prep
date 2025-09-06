e9c5d59eaf9448e659d3e1162fb2e72c5fea53f8
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-06 18:42:27 +0530

    Update print3Sum.cpp

--- Tutorials/ArrayTUT/Striver/hardProblems/print3Sum.cpp
+++ Tutorials/ArrayTUT/Striver/hardProblems/print3Sum.cpp
@@ -27,3 +27,46 @@ public:
       return ans; 
     }
 };
+**********************************************************************************************
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    set<vector<int>> st;
+
+    // check all possible triplets:
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (arr[i] + arr[j] + arr[k] == 0) {
+                    vector<int> temp = {arr[i], arr[j], arr[k]};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+            }
+        }
+    }
+ vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+**************************************************************************
+vector<vector<int>> triplet(int n, vector<int> &arr) {
+    set<vector<int>> st;
+
+    for (int i = 0; i < n; i++) {
+        set<int> hashset;
+        for (int j = i + 1; j < n; j++) {
+            //Calculate the 3rd element:
+            int third = -(arr[i] + arr[j]);
+
+            //Find the element in the set:
+            if (hashset.find(third) != hashset.end()) {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+        }
+    }
+
+    //store the set in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
