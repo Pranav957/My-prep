0e16b2c2911dcff4b4e72e12951d4930d2825ae8
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-07 16:14:17 +0530

    Create numberOfInversions.cpp

---
+++ Tutorials/ArrayTUT/Striver/hardProblems/numberOfInversions.cpp
@@ -0,0 +1,65 @@
+int numberOfInversions(vector<int>&a, int n) {
+
+    // Count the number of pairs:
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) cnt++;
+        }
+    }
+    return cnt;
+}
+*******************************************************************8888
+  int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; 
+    int left = low;     
+    int right = mid + 1; 
+   
+    int cnt = 0;
+
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1); //Modification 2
+            right++;
+        }
+    }
+
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt; // Modification 3
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt += mergeSort(arr, low, mid);  // left half
+    cnt += mergeSort(arr, mid + 1, high); // right half
+    cnt += merge(arr, low, mid, high);  // merging sorted halves
+    return cnt;
+}
+
+int numberOfInversions(vector<int>&a, int n) {
+
+    // Count the number of pairs:
+    return mergeSort(a, 0, n - 1);
+}
