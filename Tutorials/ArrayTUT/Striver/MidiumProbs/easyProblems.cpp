32b6a0975cecaee302d7ab61f97a77903b1c3c06
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-09 12:51:26 +0530

    Update easyProblems.cpp

--- Tutorials/ArrayTUT/Striver/MidiumProbs/easyProblems.cpp
+++ Tutorials/ArrayTUT/Striver/MidiumProbs/easyProblems.cpp
@@ -19,3 +19,67 @@ class Solution {
         return ans;
     }
 };
+******************************************************************
+int secondSmallest(int arr[],int n)
+{
+    if(n<2)
+        return -1;
+    int small = INT_MAX;
+    int second_small = INT_MAX;
+    int i;
+    for(i = 0; i < n; i++) 
+    {
+       if(arr[i] < small)
+       {
+          second_small = small;
+          small = arr[i];
+       }
+       else if(arr[i] < second_small && arr[i] != small)
+       {
+          second_small = arr[i];
+       }
+    }
+   return second_small;     
+}
+int secondLargest(int arr[],int n)
+{
+	if(n<2)
+	return -1;
+    int large=INT_MIN,second_large=INT_MIN;
+    int i;
+    for (i = 0; i < n; i++) 
+    {
+        if (arr[i] > large) 
+        {
+            second_large = large;
+            large = arr[i];
+        }
+ 
+        else if (arr[i] > second_large && arr[i] != large) 
+        {
+            second_large = arr[i];
+        }
+    }
+    return second_large;                
+}
+*******************************************************************************
+bool isSorted(int arr[], int n) {
+  for (int i = 1; i < n; i++) {
+    if (arr[i] < arr[i - 1])
+      return false;
+  }
+
+  return true;
+}
+************************************************8
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
