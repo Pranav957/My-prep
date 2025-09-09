cb965bdf12d1be3e77cfca512e2387a964cb3d4a
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-09 16:12:06 +0530

    Update easyProblems.cpp

--- Tutorials/ArrayTUT/Striver/MidiumProbs/easyProblems.cpp
+++ Tutorials/ArrayTUT/Striver/MidiumProbs/easyProblems.cpp
@@ -83,3 +83,131 @@ int removeDuplicates(int arr[], int n)
   }
   return i + 1;
 }
+**************************************************************************
+void Rotatetoleft(int arr[], int n, int k)
+{
+  if (n == 0)
+    return;
+  k = k % n;
+  if (k > n)
+    return;
+  int temp[k];
+  for (int i = 0; i < k; i++)
+  {
+    temp[i] = arr[i];
+  }
+  for (int i = 0; i < n - k; i++)
+  {
+    arr[i] = arr[i + k];
+  }
+  for (int i = n - k; i < n; i++)
+  {
+    arr[i] = temp[i - n + k];
+  }
+}
+#################################################################################
+void Rotateeletoleft(int arr[], int n, int k)
+{
+  
+  Reverse(arr, 0, k - 1);
+  // Reverse last n-k elements
+  Reverse(arr, k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+*******************************************************************
+vector<int> moveZeros(int n, vector<int> a) {
+    int j = -1;
+    //place the pointer j:
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    //no non-zero elements:
+    if (j == -1) return a;
+
+    //Move the pointers i and j
+    //and swap accordingly:
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
+}
+
+****************************************************
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; 
+  vector < int > Union; 
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) 
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
+##########################################################################
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  set < int > s;
+  vector < int > Union;
+  for (int i = 0; i < n; i++)
+    s.insert(arr1[i]);
+  for (int i = 0; i < m; i++)
+    s.insert(arr2[i]);
+  for (auto & it: s)
+    Union.push_back(it);
+  return Union;
+}
+***********************************************************************
+class Solution {
+  public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+      
+        
+        int i=0, j=0;
+        int m=a.size(), n=b.size();
+        vector<int> ans;
+        
+        while(i<m && j<n) {
+        //     if(i > 0 && a[i - 1] == a[i]) {  if intersection array should not have duplicates
+        //     i++;
+        //     continue;
+        // }
+            if(a[i] < b[j]) {
+                i++;
+            } else if(b[j] < a[i]) {
+                j++;
+            } else {
+                ans.push_back(a[i]); // intersection element
+                i++;
+                j++;
+            }
+        }
+        return ans;
+}
+
