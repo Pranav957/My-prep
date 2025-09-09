e8f520dcc90dc48d2f145e0d974acd779af58d27
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-09 17:15:53 +0530

    Create largestSubarraywithSumk.cpp

---
+++ Tutorials/ArrayTUT/Striver/MidiumProbs/largestSubarraywithSumk.cpp
@@ -0,0 +1,54 @@
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); 
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        long long rem = sum - k;
+
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+*************************************************************************************
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; 
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
+    }
+
+    return maxLen;
+}
