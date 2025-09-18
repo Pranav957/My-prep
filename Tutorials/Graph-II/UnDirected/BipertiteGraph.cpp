eb22e3946bb2d4a1ce1bb409296d7b47439bac74
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-09-18 13:27:14 +0530

    Update BipertiteGraph.cpp

--- Tutorials/Graph-II/UnDirected/BipertiteGraph.cpp
+++ Tutorials/Graph-II/UnDirected/BipertiteGraph.cpp
@@ -23,3 +23,41 @@ bool isBipartite(vector<vector<int>>& graph) {
     }
     return true;
 }
+******************************************************************************
+class Solution {
+public:
+
+   bool helper(vector<vector<int>>& graph,int n,vector<int>& color,int cl,int sv)
+   {
+      color[sv]=cl;
+
+      for(int i=0;i<n;i++)
+      {
+        if(graph[sv][i]==1 )
+         {
+            if(color[i]==cl)
+             return false;
+           if(color[i]==-1) 
+            {
+            bool res= helper(graph,n,color,!cl,i);
+            if(res==false)
+             return false;
+            }
+         } 
+      }
+      return true;
+   }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++)
+        {
+            if(color[i]==-1)
+            {
+                if(helper(graph,n,color,0,i)==false)
+                 return false;
+            }  
+        }
+        return true;
+    }
+};
