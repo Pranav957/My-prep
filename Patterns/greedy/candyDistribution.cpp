2eec7f8b172f9fc02f559f5dd88f4259314acdbe
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-10-07 21:41:28 +0530

    Update candyDistribution.cpp

--- Patterns/greedy/candyDistribution.cpp
+++ Patterns/greedy/candyDistribution.cpp
@@ -48,3 +48,37 @@ int candy(vector<int>& ratings) {
         return ans+ max(left[n-1],1);
 
     }
+**********************************************************************************************************************
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        int sum=1,i=1;
+       
+        while(i<n)
+        {
+            if(ratings[i]==ratings[i-1])
+            {
+              sum+=1;i++; continue;
+            }
+             int  peak=1;
+            if(ratings[i]>ratings[i-1])
+            {
+                peak+=1;
+                sum+=peak;
+            } 
+          int down=1;
+           if(ratings[i]<ratings[i-1])
+           {
+             sum+=down;
+             down+=1;
+           } 
+           i++;
+           
+           if(down > peak)
+             sum+=down-peak;
+    
+        }
+        return sum;
+    }
+};
