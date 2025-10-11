56c12922705a9e2fcebbde2f313aa5be23bd14c4
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-10-11 11:41:34 +0530

    Create maxConsecutiveOnes.cpp

---
+++ Patterns/SlidingWindow/strivers/maxConsecutiveOnes.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int longest=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            int zeros=0;
+            for(int j=i;j<nums.size();j++)
+            {
+                if(nums[j]==0)
+                 zeros++;
+              if(zeros<=k)
+                 longest=max(longest,j-i+1);
+              else    
+                break;
+                
+            }
+        }
+        return longest;
+    }
+};
+********************************************************************************************
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int longest=0;
+        int left=0;
+        int zeros=0;
+        for(int r=0;r<nums.size();r++)
+        {
+          if(nums[r]==0)
+          {
+             zeros++;
+          }
+          if(zeros<=k)
+          {
+             longest=max(longest,r-left+1);
+          }
+          else
+            {
+                while(nums[left]!=0)
+                 left++;
+               left++;
+               zeros--;  
+            }
+        }
+        return longest;
+    }
+};
+****************************************************************
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int longest=0;
+        int left=0;
+        int zeros=0;
+        for(int r=0;r<nums.size();r++)
+        {
+          if(nums[r]==0)
+          {
+             zeros++;
+          }
+          if(zeros<=k)
+          {
+             longest=max(longest,r-left+1);
+          }
+          else
+            {
+                if(nums[left]==0)
+                 zeros--;  
+               left++;
+               
+            }
+        }
+        return longest;
+    }
+};
