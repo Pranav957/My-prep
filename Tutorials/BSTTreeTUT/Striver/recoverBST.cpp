b1631233822823a3d7f1bd9fa79846ead3a76a72
Pranav Kulkarni <68426166+Pranav957@users.noreply.github.com>
2025-11-19 13:17:52 +0530

    Update recoverBST.cpp

--- Tutorials/BSTTreeTUT/Striver/recoverBST.cpp
+++ Tutorials/BSTTreeTUT/Striver/recoverBST.cpp
@@ -35,3 +35,38 @@ public:
         }
     }
 };
+
+*****************************************************************************
+class Solution {
+    Node* prev=NULL;
+    Node* first=NULL;
+    Node*second=NULL;
+  public:
+   void inorder(Node* root)
+   {
+       if(root==NULL)
+        return;
+        
+        inorder(root->left);
+        
+        if(prev!=NULL)
+        {
+            if(prev->data>root->data)
+             {
+                 if(first==NULL)
+                  first=prev;
+                  second=root;
+             }
+        }
+        prev=root;
+        
+        inorder(root->right);
+   }
+    void correctBST(Node* root) {
+        // add code here.
+        inorder(root);
+        int temp=first->data;
+     first->data=second->data;
+     second->data=temp;
+    }
+};
